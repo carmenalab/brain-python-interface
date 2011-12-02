db61f0ae2946874aac397abe518f3ce13b3810df
Helene <helene@nucleus.(none)>
2011-12-01 16:46:50 -0800

    Adjust the options for visual_response

--- visual_response.py
+++ visual_response.py
@@ -3,11 +3,11 @@ from riglib.tasks import Dots
 
 if __name__ == "__main__":
     options = {
-        "flat_proportion": 0.5, 
+        "flat_proportion": 0, 
         "penalty_time": 5,
         "ignore_time": 4, 
         "rand_start": (1, 10), 
         "reward_time": 5, 
-        "timeout_time": 4
+        "timeout_time": 3
     }
     consolerun(Dots, ("autostart","button","ignore_correctness"), **options)
