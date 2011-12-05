1e0e18b37fc81e480d8278d8fb9703f8890d9e14
James Gao <james@jamesgao.com>
2011-12-05 15:33:12 -0800

    Add a red penalty screen

--- riglib/tasks/dots.py
+++ riglib/tasks/dots.py
@@ -21,7 +21,11 @@ class Dots(Pygame.Pygame):
         rc = 3*self.width / 4 - mask.shape[1] / 2
 
         self.mask = mask
-        self.coords = (lc, mid), (rc, mid)
+        self.coords = (lc, mid), (rc, mid
+    
+    def _while_penalty(self):
+        self.surf.fill((255,0,0))
+        pygame.display.flip()
     
     def draw_frame(self):
         self.surf.blit(self.sleft, self.coords[0])
