f0f5510961fed7bd646cf991258d59988eee1213
James Gao <james@jamesgao.com>
2011-12-06 16:55:48 -0800

    Adding trial reporting

--- visual_response.py
+++ visual_response.py
@@ -10,4 +10,8 @@ if __name__ == "__main__":
         "timeout_time": 3,
         "trial_probs": [0, None], 
     }
+<<<<<<< HEAD
     exp = consolerun(Dots, ("autostart","button","ignore_correctness"), **options)
+=======
+    exp = consolerun(Dots, ("autostart","button","ignore_correctness"), **options)
+>>>>>>> Adding trial reporting
