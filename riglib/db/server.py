0ee192d4379f416110458b5599585988ca0b91c2
james <james@arcturus.(none)>
2011-12-07 19:48:51 -0800

    Add the latest dbserver

--- riglib/db/server.py
+++ riglib/db/server.py
@@ -3,15 +3,26 @@ import os
 import tornado.ioloop
 import tornado.web
 
-from riglib.experiment import featurelist
-from riglib.tasks import tasklist
+from ..experiment import featurelist
+from ..tasks import tasklist
 
 cwd = os.path.split(os.path.abspath(__file__))[0]
 
 class CreateRecord(tornado.web.RequestHandler):
     def get(self):
-    	alltraits = [ [for trait in task.class_editable_traits()] for name, task in tasklist.items()]
-        self.render("resources/start.html", features=featurelist, tasks=tasklist)
+        alltraits = []
+        for name, task in tasklist.items():
+            tlist = []
+            traits = task.class_traits()
+            for trait in task.class_editable_traits():
+                if isinstance(traits[trait], tuple):
+                    tlist.append((trait, len(traits[trait].default), traits[trait].desc))
+                else:
+                    tlist.append((trait, 1, traits[trait].desc))
+            alltraits.append(tlist)
+
+    	print alltraits
+        self.render("resources/start.html", features=featurelist, tasks=tasklist, alltraits=alltraits)
 
 class AJAXHandler(tornado.web.RequestHandler):
     def get(self, path):
---
+++ riglib/tasks/__init__.py
@@ -0,0 +1,7 @@
+from rds import RDS
+from dots import Dots
+
+tasklist = dict(
+	dots=Dots,
+	rds=RDS,
+)
