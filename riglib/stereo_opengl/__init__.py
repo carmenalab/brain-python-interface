e0d40b6cb3132963f74d65076cc52ff31b4e6681
James Gao <james@jamesgao.com>
2012-02-22 20:16:58 -0800

    Integrating LogExperiment into the Window class

--- riglib/stereo_opengl/__init__.py
+++ riglib/stereo_opengl/__init__.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
-    from window import Test
+    from window import Window
     from primitives import Cylinder, Plane
-    win = Test()
-    win.add_model(Cylinder())
+    win = Window()
+    win.add_model(Cylinder(height=2).translate(0,0,-1))
     win.start()
--- riglib/stereo_opengl/window.py
+++ riglib/stereo_opengl/window.py
@@ -1,54 +1,54 @@
+import os
 import threading
+import operator
 
 import pygame
 import numpy as np
 from OpenGL.GL import *
 
+from riglib.experiment import LogExperiment, traits
+
 from world import Context, perspective
 from models import Group
-'''
-from riglib.experiment import LogExperiment, traits
+
+cwd = os.path.abspath(os.path.split(__file__)[0])
+
 class Window(LogExperiment):
-    background = (0,0,0)
+    status = dict(draw=dict(stop=None))
+    state = "draw"
+    stop = False
+
+    background = (0,0,0,1)
     fps = 60
+    fov = 90
 
-    def __init__(self, world, *args, **kwargs):
-        super(Window, self).__init__(*args, **kwargs)
-        self.world = world
-    
-    def screen_init(self):
-        pygame.init()
-
-        flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.OPENGL
-        pygame.display.set_mode((800,600), flags)
-        
-    def draw(self):
-        pass
-'''
-eye = [0, -2, 0.5]
-class Test(threading.Thread):
     def __init__(self, *args, **kwargs):
-        super(Test, self).__init__()
+        super(Window, self).__init__()
         self.models = []
+        self.eyepos = [0,-2,0]
 
     def init(self):
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "1680,0"
+        os.environ['SDL_VIDEO_X11_WMCLASS'] = "monkey_experiment"
         pygame.init()
+
         flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.OPENGL
         pygame.display.set_mode((800,600), flags)
         self.clock = pygame.time.Clock()
 
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glClearColor(0.5,0.6,0.5,1)
+        glClearColor(*self.background)
         glClearDepth(1.0)
         glDepthFunc(GL_LESS)
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_TEXTURE_2D)
+        glViewport(0,0,800,600)
 
-        self.ctx = Context(open("test.v.glsl"), open("test.f.glsl"))
-        self.projection = perspective(45, 800./600, 0.0625, 256)
+        self.ctx = Context(open(os.path.join(cwd, "test.v.glsl")), open(os.path.join(cwd, "test.f.glsl")))
+        self.projection = perspective(self.fov/2, 800./600, 0.0625, 256)
         #this effectively determines the modelview matrix
-        self.world = Group(self.models).rotate_x(-90).translate(-eye[0], -eye[1], -eye[2])
+        self.world = Group(self.models).rotate_x(-90).translate(*map(operator.neg, self.eyepos))
         self.world.init()
 
     def add_model(self, model):
@@ -56,20 +56,20 @@ class Test(threading.Thread):
     
     def run(self):
         self.init()
-        run = True
-        glViewport(0,0,800,600)
-        while run:
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glUseProgram(self.ctx.program)
-
-            self.ctx.uniforms['p_matrix'] = self.projection
-            self.world.draw(self.ctx)
-
-            pygame.display.flip()
-
-            e = pygame.event.get(pygame.KEYDOWN)
-            if len(e) > 0 and e[-1].key == 27:
-                run = False
-            self.clock.tick(15)
-
-        pygame.display.quit()
+        return super(Window, self).run()
+    
+    def _get_event(self):
+        for e in pygame.event.get(pygame.KEYDOWN):
+            return (e.key, e.type)
+    
+    def _while_draw(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glUseProgram(self.ctx.program)
+        self.ctx.uniforms['p_matrix'] = self.projection
+        self.world.draw(self.ctx)
+        pygame.display.flip()
+        self.clock.tick(self.fps)
+        self.event = self._get_event()
+    
+    def _test_stop(self, ts):
+        return self.stop or self.event is not None and self.event[0] == 27
