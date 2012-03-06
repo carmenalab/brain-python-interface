798220ce3f1cd9eb1db90ce475aee97e419f5062
james <james@arcturus.(none)>
2012-03-06 15:36:24 -0800

    holy crap fucked-up indents

--- riglib/motiontracker.py
+++ riglib/motiontracker.py
@@ -5,57 +5,57 @@ from OWL import *
 class System(object):
     def __init__(self, marker_count=1,server_name='10.0.0.11', init_flags=0):
         if(owlInit(server_name, init_flags) < 0):
-	        raise Exception(owl_get_error("init error",owlGetError()))
-				
-		# flush requests and check for errors fix
-	    if(owlGetStatus() == 0):
-	        raise Exception(owl_get_error("error in point tracker setup", owlGetError()))
-		
-		# set define frequency
-	    owlSetFloat(OWL_FREQUENCY, OWL_MAX_FREQUENCY)
-	
-		#create a point tracker
-		self.tracker = 0
-	    owlTrackeri(self.tracker, OWL_CREATE, OWL_POINT_TRACKER)
-	
-		# set markers
-	    for i in range(marker_count):
-	        owlMarkeri(MARKER(self.tracker, i), OWL_SET_LED, i)
-		owlTracker(self.tracker, OWL_ENABLE)
+            raise Exception(owl_get_error("init error",owlGetError()))
+                
+        # flush requests and check for errors fix
+        if(owlGetStatus() == 0):
+            raise Exception(owl_get_error("error in point tracker setup", owlGetError()))
+        
+        # set define frequency
+        owlSetFloat(OWL_FREQUENCY, OWL_MAX_FREQUENCY)
+    
+        #create a point tracker
+        self.tracker = 0
+        owlTrackeri(self.tracker, OWL_CREATE, OWL_POINT_TRACKER)
+    
+        # set markers
+        for i in range(marker_count):
+            owlMarkeri(MARKER(self.tracker, i), OWL_SET_LED, i)
+        owlTracker(self.tracker, OWL_ENABLE)
     
     def start(self, filename=None):
-		self.filename = filename
-		if filename is not None:
-			#figure out command to tell phasespace to start a recording
-			pass
+        self.filename = filename
+        if filename is not None:
+            #figure out command to tell phasespace to start a recording
+            pass
         owlSetInteger(OWL_STREAMING, OWL_ENABLE)
 
     def stop(self):
-		if self.filename is not None:
-			#tell phasespace to stop recording
-			pass
-		owlSetInteger(OWL_STREAMING, OWL_DISABLE)
+        if self.filename is not None:
+            #tell phasespace to stop recording
+            pass
+        owlSetInteger(OWL_STREAMING, OWL_DISABLE)
     
     def get(self):
-		markers=[]
-		coords = np.zeros((32, 3))
-	    n = owlGetMarkers(markers, 32)
-		for i in range(n):
-			if markers[i].cond > 0:
-				coords[i] = markers[i].x, markers[i].y, markers[i].z
-		return coords
-		
+        markers=[]
+        coords = np.zeros((32, 3))
+        n = owlGetMarkers(markers, 32)
+        for i in range(n):
+            if markers[i].cond > 0:
+                coords[i] = markers[i].x, markers[i].y, markers[i].z
+        return coords
+        
     def retrieve(self, filename):
-		pass
+        pass
     
     def sendMsg(self, msg):
-		pass
+        pass
 
     def __del__(self):
-		for i in range(marker_count):
-	        owlMarker(MARKER(self.tracker, i), OWL_CLEAR_MARKER)   
-	 	self.owlTracker(self.tracker, OWL_DESTROY)
-	  	owlDone()
+        for i in range(marker_count):
+            owlMarker(MARKER(self.tracker, i), OWL_CLEAR_MARKER)   
+        self.owlTracker(self.tracker, OWL_DESTROY)
+        owlDone()
 
 def owl_get_error(s, n):
     """Print OWL error."""
