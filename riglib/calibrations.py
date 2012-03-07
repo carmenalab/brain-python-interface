b8a13001f25ee10f570df696e7469ed1949677bd
James Gao <james@jamesgao.com>
2012-03-07 15:59:08 -0800

    Tweak the stereo projection some more, almost figured out everything now...

---
+++ riglib/calibrations.py
@@ -0,0 +1,16 @@
+from scipy.interpolate import Rbf
+
+class ThinPlate(object):
+    '''Interpolates arbitrary input dimensions into arbitrary output dimensions using thin plate splines'''
+    def __init__(self, data, actual, smooth=0):
+        self.data = data
+        self.actual = actual
+        self.smooth = smooth
+
+        self.funcs = []
+        for a in actual.T:
+            f = Rbf(*np.vstack([data.T, a]), function='thin_plate', smooth=smooth)
+            self.funcs.append(f)
+
+    def __call__(self, data):
+        return np.vstack([func(d) for func, d in zip(self.funcs, data.T)]).T
--- riglib/stereo_opengl/render/stereo.py
+++ riglib/stereo_opengl/render/stereo.py
@@ -4,18 +4,36 @@ from OpenGL.GL import *
 from render import Renderer
 from utils import offaxis_frusta, mirror_frusta
 
-class RightLeft(Renderer):
+class LeftRight(Renderer):
     def __init__(self, window_size, fov, near, far, focal_dist, iod, **kwargs):
         w, h = window_size
-        super(RightLeft, self).__init__((w/2,h), fov, near, far, **kwargs)
+        super(LeftRight, self).__init__((w/2,h), fov, near, far, **kwargs)
         self.projections = offaxis_frusta((w/2, h), fov, near, far, focal_dist, iod)
     
     def draw(self, root, **kwargs):
         w, h = self.size
-        glViewport(w, 0, w, h)
-        super(RightLeft, self).draw(root, p_matrix=self.projections[0], **kwargs)
         glViewport(0, 0, w, h)
-        super(RightLeft, self).draw(root, p_matrix=self.projections[1], **kwargs)
+        super(LeftRight, self).draw(root, p_matrix=self.projections[0], **kwargs)
+        glViewport(w, 0, w, h)
+        super(LeftRight, self).draw(root, p_matrix=self.projections[1], **kwargs)
+
+class RightLeft(LeftRight):
+    def draw(self, root, **kwargs):
+        w, h = self.size
+        glViewport(w, 0, w, h)
+        super(LeftRight, self).draw(root, p_matrix=self.projections[0], **kwargs)
+        glViewport(0, 0, w, h)
+        super(LeftRight, self).draw(root, p_matrix=self.projections[1], **kwargs)
+
+class MirrorDisplay(LeftRight):
+    '''The mirror display requires a left-right flip, otherwise the sides are messed up'''
+    def __init__(self, *args, **kwargs):
+        super(MirrorDisplay, self).__init__(*args, **kwargs)
+        flip = np.eye(4)
+        flip[0,0] = -1
+
+        self.projections = [np.dot(p, flip) for p in self.projections]
+
 
 class Anaglyph(Renderer):
     def __init__(self, window_size, fov, near, far, focal_dist, iod, **kwargs):
@@ -30,19 +48,3 @@ class Anaglyph(Renderer):
         glColorMask(GL_FALSE, GL_TRUE, GL_TRUE, GL_TRUE)
         super(Anaglyph, self).draw(root, p_matrix=self.projections[1], **kwargs)
         glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE)
-
-class MirrorDisplay(Renderer):
-    '''The mirror display requires a left-right flip, otherwise the sides are messed up'''
-    def __init__(self, window_size, fov, near, far, focal_dist, iod, **kwargs):
-        w, h = window_size
-        super(MirrorDisplay, self).__init__((w/2,h), fov, near, far, **kwargs)
-        self.projections = mirror_frusta((w/2, h), fov, near, far, focal_dist, iod)
-    
-    def draw(self, root, **kwargs):
-        w, h = self.size
-        glViewport(0, 0, w, h)
-        super(MirrorDisplay, self).draw(root, p_matrix=self.projections[0], **kwargs)
-        glViewport(w, 0, w, h)
-        super(MirrorDisplay, self).draw(root, p_matrix=self.projections[1], **kwargs)
-
-
