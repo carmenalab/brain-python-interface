fe0e4edf888180087d792b8424a4304a1250a233
Helene <helene@nucleus.(none)>
2012-04-28 14:03:57 -0700

    cPickle requires string, not unicode

--- db/tracker/models.py
+++ db/tracker/models.py
@@ -117,7 +117,7 @@ class Sequence(models.Model):
         from json_param import Parameters
         if self.generator.static:
             if len(self.sequence) > 0:
-                return generate.runseq, dict(seq=cPickle.loads(self.sequence))
+                return generate.runseq, dict(seq=cPickle.loads(str(self.sequence)))
             return generate.runseq, dict(seq=self.generator.get()(**Parameters(self.params).params))
 
         return self.generator.get(), Parameters(self.params).params
