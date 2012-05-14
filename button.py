9748d074480448e8cc46d6304f49c8d445c12ffe
james <james@arcturus.(none)>
2012-05-13 23:51:45 -0700

    Moving stuff around a bit, add a new diagram

--- button.py
+++
@@ -1,7 +0,0 @@
-from riglib.experiment import consolerun
-from tasks.button import ButtonTask
-
-if __name__ == "__main__":
-	probs = [0, 0, 1]
-	#use [0, 1, 0] for right side, [1, 0, 0] for left side, [0, 0, 1] for either side
-	exp = consolerun(ButtonTask, ("button",), probs=probs)
