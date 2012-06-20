887b0420bc1afae0fd183c1c6ee3eb7278ee6e80
James Gao <james@jamesgao.com>
2012-06-20 16:26:44 -0700

    Fix up the sequence link

--- db/tracker/views.py
+++ db/tracker/views.py
@@ -24,8 +24,14 @@ def list(request):
     return render_to_response('list.html', fields, RequestContext(request))
 
 def get_sequence(request, idx):
+    import cPickle
     entry = TaskEntry.objects.get(pk=idx)
-    response = HttpResponse(entry.sequence.sequence, content_type='application/x-pickle')
+    seq = cPickle.loads(str(entry.sequence.sequence))
+    log = json.loads(entry.report)
+    num = len([l[2] for l in log if l[0] == "wait"])
+
+    response = HttpResponse(cPickle.dumps(seq[:num]), content_type='application/x-pickle')
     response['Content-Disposition'] = 'attachment; filename={subj}{time}.pkl'.format(
-        subj=entry.subject.name, time="%04d%02d%02d"%(entry.date.year, entry.date.month, entry.date.day))
+        subj=entry.subject.name[:4].lower(), 
+        time="%04d%02d%02d"%(entry.date.year, entry.date.month, entry.date.day))
     return response
