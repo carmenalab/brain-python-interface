bdc74fbbc2b9ab3da971baf14748ab8c5711143d
Helene <helene@nucleus.(none)>
2013-08-23 13:26:38 -0700

    test case for training KFDecoder from manual control data

--- tests/test_decoder_training.py
+++ tests/test_decoder_training.py
@@ -3,13 +3,12 @@ from riglib.bmi import train
 test_mc = True
 test_bc = False
 if test_mc:
-    block = 'cart20130428_01'
-    #block = 'cart20130425_05'
+    block = 'cart20130815_01'
     files = dict(plexon='/storage/plexon/%s.plx' % block, hdf='/storage/rawdata/hdf/%s.hdf' % block)
     binlen = 0.1
     tslice = [1., 300.]
     
-    decoder = _train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None],
+    decoder = train._train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None],
         state_vars=['hand_px', 'hand_pz', 'hand_vx', 'hand_vz', 'offset'], **files) 
 if test_bc:
     block = 'cart20130521_04'
