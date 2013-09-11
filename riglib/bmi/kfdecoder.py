dcd61325b78083e243ea559586967bfa47ab4635
Helene <helene@nucleus.(none)>
2013-09-11 15:11:49 -0700

    finished adding 'bin_edges' variable to task data

--- riglib/bmi/kfdecoder.py
+++ riglib/bmi/kfdecoder.py
@@ -409,10 +409,11 @@ class KFDecoder(BMI):
         # "Bin" spike timestamps to generate spike counts
         if len(ts_data_k) == 0:
             spike_counts = np.zeros((self.bin_spikes.nunits,))
-            task_data['bin_edges'] = np.array([nan, nan])
+            task_data['bin_edges'] = np.array([np.nan, np.nan])
         elif ts_data_k.dtype == python_plexnet_dtype:
             spike_counts = self.bin_spikes(ts_data_k)
-            task_data['bin_edges'] = np.array([np.min(ts_data_k), np.max(ts_data_k)])
+            temp = np.array([e[0] for e in ts_data_k])
+            task_data['bin_edges'] = np.array([np.min(temp), np.max(temp)])
         else:
             spike_counts = ts_data_k
 
