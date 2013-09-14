c42353f45608d8e57ccf60dfe8042838787f7594
Helene Moorman <helenem@HMSDH.(none)>
2013-09-14 10:58:51 -0700

    went back to sliding window but now binning spikes based on nucleus' clock instead of relying on plx timestamps

--- riglib/bmi/kfdecoder.py
+++ riglib/bmi/kfdecoder.py
@@ -387,7 +387,7 @@ class KFDecoder(BMI):
         return self.predict(obs_t, **kwargs)
 
 
-    def predict(self, ts_data_k, target=None, speed=1.0, target_radius=0.5,
+    def predict(self, spike_counts, target=None, speed=1.0, target_radius=0.5,
                 assist_level=0.0, dt=0.1, task_data=None, **kwargs):
         """Decode the spikes"""
         # Save the previous cursor state for assist
@@ -405,18 +405,6 @@ class KFDecoder(BMI):
 
             assist_cursor_vel = (assist_cursor_pos-cursor_pos)/dt;
             assist_cursor_kin = np.hstack([assist_cursor_pos, assist_cursor_vel, 1])
-
-        # "Bin" spike timestamps to generate spike counts
-        if len(ts_data_k) == 0:
-            spike_counts = np.zeros((self.bin_spikes.nunits,))
-            task_data['bin_edges'] = np.array([np.nan, np.nan])
-        elif ts_data_k.dtype == python_plexnet_dtype:
-            spike_counts = self.bin_spikes(ts_data_k)
-            min_ind = np.argmin(ts_data_k['ts'])
-            max_ind = np.argmax(ts_data_k['ts'])
-            task_data['bin_edges'] = np.array([ts_data_k[min_ind][0], ts_data_k[max_ind][0]])
-        else:
-            spike_counts = ts_data_k
 
         if task_data is not None:
             task_data['bins'] = spike_counts
