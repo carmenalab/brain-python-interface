changed units for everything from mm to cm, fixed generator argument bug in interface, got rid of exit radius

riglib/bmi/kfdecoder.py
@@ -359,6 +359,7 @@ class KalmanFilter():
 
 
 class KFDecoder(BMI):
+    dist_units = 'cm'
     def __init__(self, kf, mFR, sdFR, units, bounding_box, states, drives_neurons,
         states_to_bound, binlen=0.1, tslice=[-1,-1]):
         """ Initializes the Kalman filter decoder.  Includes BMI specific
@@ -428,7 +429,7 @@ class KFDecoder(BMI):
 
         return self.predict(obs_t, **kwargs)
 
-    def predict(self, spike_counts, target=None, speed=6.0, target_radius=20,
+    def predict(self, spike_counts, target=None, speed=0.6, target_radius=2,
                 assist_level=0.0, task_data=None, assist_inds=[0,1,2],
                 **kwargs):
         """Decode the spikes"""
@@ -447,9 +448,6 @@ class KFDecoder(BMI):
 
             assist_cursor_vel = (assist_cursor_pos-cursor_pos)/self.binlen
             assist_cursor_kin = np.hstack([assist_cursor_pos, assist_cursor_vel, 1])
-
-        if task_data is not None:
-            task_data['bins'] = spike_counts
 
         # re-normalize the variance of the spike observations, if nec
         if self.zscore:

riglib/bmi/train.py
@@ -65,7 +65,7 @@ def _train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None],
         raise ValueError('Training data and neural data are the wrong length: %d vs. %d'%(len(kin), len(neurons)))
     
     # Match kinematics with the task state
-    task_states = ['origin_hold', 'terminus', 'terminus_hold', 'reward']
+    task_states = ['target', 'hold']
     states = h5.root.motiontracker_msgs[:]
     # assign names to each of the states
     state_numbering = dict((n, i) for i, n in enumerate(np.unique(states['msg'])))
@@ -94,7 +94,7 @@ def _train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None],
     kin = np.ma.hstack([kin[:-1,:,:3], velocity])
     
     hand_kin = kin[:, [14,kin.shape[1]/2+14], :]
-    hand_kin = hand_kin.reshape(len(hand_kin), -1)
+    hand_kin = hand_kin.reshape(len(hand_kin), -1)/10.0 #convert motiontracker kinematic data to cm
     
     # train KF model parameters
     neurons = neurons.T
@@ -219,7 +219,8 @@ def _train_KFDecoder_visual_feedback(cells=None, binlen=0.1, tslice=[None,None],
     sdFR = sdFR[unit_inds]
 
     # instantiate KFdecoder
-    bounding_box = np.array([-250., -140.]), np.array([250., 140.])
+    #bounding_box = np.array([-250., -140.]), np.array([250., 140.])
+    bounding_box = np.array([-25., -14.]), np.array([25., 14.]) # bounding box in cm
     states_to_bound = ['hand_px', 'hand_pz']
     neuron_driving_states = ['hand_vx', 'hand_vz', 'offset']
     drives_neurons = np.array([x in neuron_driving_states for x in state_vars])