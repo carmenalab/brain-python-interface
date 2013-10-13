c744c334b1463cf2f43441d265f1ae6ab8c523f9
Suraj Gowda <surajgowda33@gmail.com>
2013-10-13 10:25:58 -0700

    point process sim neurons working (matches MATLAB)

--- riglib/bmi/sim_neurons.py
+++ riglib/bmi/sim_neurons.py
@@ -13,8 +13,7 @@ from numpy.random import poisson, rand
 from scipy.io import loadmat, savemat
 
 from scipy.integrate import trapz, simps
-#import statsmodels.api as sm # TODO add back in for point process ensemble
-
+import statsmodels.api as sm # TODO add back in for point process ensemble
 
 ts_dtype = [('ts', float), ('chan', np.int32), ('unit', np.int32)]
 class CosEnc():
@@ -86,3 +85,137 @@ class CosEnc():
             'angles':self.angles, 'pds':self.pds, 'mod_depth':self.mod_depth,
             'unit_inds':self.unit_inds})
 
+
+class PointProcess():
+    def __init__(self, beta, dt, tau_samples=[], K=0, eps=1e-3):
+        self.beta = beta.reshape(-1, 1)
+        self.dt = dt
+        self.tau_samples = tau_samples
+        self.K = K
+        self.eps = eps
+        self.i = 0
+        self.j = self.i + 1
+        self.X = np.zeros([0, len(beta)])
+        self._reset_res()
+        self.tau = np.inf
+        self.rate = np.nan
+
+    def _exp_sample(self):
+        if len(self.tau_samples) > 0:
+            self.tau = self.tau_samples.pop(0)
+        else:
+            print "drawing new samples"
+            u = np.random.rand()
+            self.tau = np.log(1 - u);
+
+    def _reset_res(self):
+        self.resold = 1000
+        self.resnew = np.nan
+
+    def _integrate_rate(self):
+        # integrate rate
+        loglambda = np.dot(self.X[self.last_spike_ind:self.j+1, :], self.beta) #log of lambda delta
+        self.rate = np.ravel(np.exp(loglambda)/self.dt)
+
+        if len(self.rate) > 2:
+            self.resnew = self.tau + simps(self.rate, dx=self.dt, even='first')
+        else:
+            self.resnew = self.tau + trapz(self.rate, dx=self.dt)
+
+    def _decide(self):
+        if (self.resold > 0) and (self.resnew > self.resold):
+            return True
+        else:
+            #self.j = self.j + 1;
+            self.resold = self.resnew;
+            return False
+
+    def _push(self, x_t):
+        self.X = np.vstack([self.X, x_t])
+
+    def __call__(self, x_t):
+        self._push(x_t)
+        if np.abs(self.resold) < self.eps:
+            spiking_bin = True
+        else:
+            self._integrate_rate()
+            spiking_bin = self._decide()
+
+        # Handle the spike
+        if spiking_bin:
+            self.last_spike_ind = self.j - 1
+            self._reset_res()
+            self._exp_sample()
+            self._integrate_rate()
+            self.resold = self.resnew;
+
+        self.j += 1
+        return spiking_bin
+
+    def _init_sampling(self, x_t):
+        self._push(x_t) # initialize the observed extrinsic covariates
+        self._reset_res()
+        self._exp_sample()
+        self.j = 1
+        self.last_spike_ind = 0 # initialization
+
+    def sim_batch(self, X, verbose=False):
+        framelength = X.shape[0]
+        spikes = np.zeros(framelength);
+
+        self._init_sampling(X[0,:])
+    
+        while self.j < framelength:
+            #spiking_bin = self(X[self.j, :])
+            spiking_bin = self.__call__(X[self.j, :])
+            if self.j < framelength and spiking_bin:
+                spikes[self.last_spike_ind] = 1;
+
+        return spikes
+
+
+class PointProcessEnsemble():
+    def __init__(self, beta, init_state, dt, tau_samples=None, eps=1e-3, 
+                 hist_len=0, unit_inds=None):
+        '''
+        Initialize an ensemble of point-process neurons
+        '''
+        self.n_neurons = beta.shape[1]
+        if unit_inds == None:
+            unit_inds = np.arange(1, self.n_neurons+1)
+        self.unit_inds = unit_inds
+
+        if tau_samples == None:
+            tau_samples = [[]] * self.n_neurons
+
+        # TODO the individual point-processes need to be initialized with some 
+        # starting kinematic state (_init_sampling method)
+        self.units = []
+        for k in range(self.n_neurons):
+            point_proc = PointProcess(beta[:,k], dt, tau_samples[k])
+            point_proc._init_sampling(init_state)
+            self.units.append(point_proc)
+        #self.units = [ for k in range(self.n_neurons)]
+        self.include_offset = np.all(beta[0,:] == 1) # TODO Convention
+
+    def __call__(self, user_input):
+        user_input = np.hstack([1, user_input]) # TODO convention needs to go back to offset state last ...
+        return np.array([unit(user_input) for unit in self.units]).astype(np.float64).reshape(-1,1)
+
+
+class PointProcessEnsemble():
+    def __init__(self, beta, init_state, dt, tau_samples=None, eps=1e-3, 
+                 hist_len=0, unit_inds=None):
+        '''
+        Initialize a point process ensemble
+        '''
+        self.n_neurons = beta.shape[1]
+        units = []
+        for k in range(self.n_neurons):
+            point_proc = PointProcess(beta[:,k], dt, tau_samples=tau_samples[k])
+            point_proc._init_sampling(init_state)
+            units.append(point_proc)
+        self.units = units
+
+    def __call__(self, x_t):
+        return np.array(map(lambda unit: unit(x_t), self.units)).astype(int)
