6bb00b898c219d22eb85dbf8269f3a64877f0c7b
Suraj Gowda <surajgowda33@gmail.com>
2013-10-23 17:59:53 -0700

    minor cleanup

--- tests/ppf/test_ppfdecoder.py
+++ tests/ppf/test_ppfdecoder.py
@@ -8,8 +8,7 @@ import numpy as np
 from scipy.io import loadmat, savemat
 from riglib.bmi import sim_neurons
 import matplotlib.pyplot as plt
-from riglib.bmi import ppfdecoder, state_space_models, train
-from scipy.io import loadmat, savemat
+from riglib.bmi import ppfdecoder, train
 
 reload(ppfdecoder)
 reload(sim_neurons)
@@ -17,9 +16,9 @@ reload(train)
 plt.close('all')
 
 N = 10000
-data = loadmat('sample_spikes_and_kinematics_%d.mat' % N)
+fname ='sample_spikes_and_kinematics_%d.mat' % N 
+data = loadmat(fname)
 truedata = loadmat('/Users/sgowda/bmi/workspace/adaptive_ppf/ppf_test_case_matlab_output.mat')
-#hand_vel = data['hand_vel']
 x_est = truedata['x_est']
 X = data['hand_vel'].T
 
@@ -30,24 +29,17 @@ n_neurons = beta.shape[0]
 n_iter = X.shape[0]
 dt = 0.005
 
-#init_state = np.hstack([X[0,:], 1])
-init_state = np.array([0., 0, 1])
-tau_samples = [data['tau_samples'][0][k].ravel().tolist() for k in range(n_neurons)]
-ensemble = sim_neurons.PointProcessEnsemble(beta, dt, init_state=init_state, tau_samples=tau_samples)
+encoder = sim_neurons.load_ppf_encoder_2D_vel_tuning(fname, dt=dt)
 
 states = ['hand_px', 'hand_py', 'hand_pz', 'hand_vx', 'hand_vy', 'hand_vz', 'offset']
 decoding_states = ['hand_vx', 'hand_vz', 'offset'] 
-
-#print np.array_equal(train.inflate(beta.T, decoding_states, states), )
 beta_dec = train.inflate(beta, decoding_states, states, axis=1)
-#beta = train.inflate(beta.T, decoding_states, states)
-
-dec = train._train_PPFDecoder_sim_known_beta(beta_dec, ensemble.units, dt=dt, dist_units='m')
+dec = train._train_PPFDecoder_sim_known_beta(beta_dec, encoder.units, dt=dt, dist_units='m')
 
 spike_counts = np.zeros([n_iter, n_neurons])
 decoded_output_new = np.zeros([7, n_iter])
 for n in range(1, n_iter):
-    spike_counts[n-1, :] = ensemble(X[n,:])
+    spike_counts[n-1, :] = encoder(X[n,:])
     decoded_output_new[:, n-1] = dec.predict(spike_counts[n-1 ,:])
 
 print "Python sim spikes matches MATLAB's: %s" % np.array_equal(spike_counts, data['spike_counts'])
@@ -58,7 +50,6 @@ print np.max(np.abs(x_est[0,:n_iter-1] - decoded_output_new[3,:-1]))
 plt.figure()
 plt.hold(True)
 plt.plot(decoded_output_new[3,:n_iter], label='pyth')
-#plt.plot(np.arange(0, n_iter, dec.n_subbins), decoded_output_new[3,:n_iter:dec.n_subbins], label='pyth')
 plt.plot(x_est[0,:n_iter], label='matlab')
 plt.plot(X[:n_iter, 0], label='handvel')
 plt.legend()
