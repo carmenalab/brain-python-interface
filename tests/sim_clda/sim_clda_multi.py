3613e7180ae79c9fb85d42b871ea436e2793d829
Suraj Gowda <surajgowda33@gmail.com>
2013-10-27 14:00:09 -0700

    Factored out simulation 'features' added simulation task for PPF

--- tests/sim_clda/sim_clda_multi.py
+++ tests/sim_clda/sim_clda_multi.py
@@ -10,37 +10,21 @@ import multiprocessing as mp
 from scipy.io import loadmat, savemat
 from riglib.experiment.features import Autostart, SimHDF
 import riglib.bmi
-from riglib.bmi import kfdecoder, clda
+from riglib.bmi import kfdecoder, clda, ppfdecoder
 from tasks import bmimultitasks, generatorfunctions as genfns
 
 reload(kfdecoder)
+reload(ppfdecoder)
 reload(clda)
 reload(riglib.bmi)
 reload(riglib.bmi.train)
 
 from riglib.stereo_opengl.window import WindowDispl2D
 
-class SimCLDAControlMultiDispl2D(WindowDispl2D, bmimultitasks.SimCLDAControlMulti, Autostart):
-    update_rate = 0.1
-    def __init__(self, *args, **kwargs):
-        self.target_radius = 1.8
-        bmimultitasks.SimCLDAControlMulti.__init__(self, *args, **kwargs)
-        self.batch_time = 0.1
-        self.half_life  = 20.0
-
-        self.origin_hold_time = 0.250
-        self.terminus_hold_time = 0.250
-        self.hdf = SimHDF()
-        self.task_data = SimHDF()
+class SimTime(object):
+    def __init__(self):
         self.start_time = 0.
         self.loop_counter = 0
-        self.assist_level = 0
-
-    def create_updater(self):
-        clda_input_queue = mp.Queue()
-        clda_output_queue = mp.Queue()
-        #self.updater = clda.KFRML(clda_input_queue, clda_output_queue, self.batch_time, self.half_life)
-        self.updater = clda.KFOrthogonalPlantSmoothbatch(clda_input_queue, clda_output_queue, self.batch_time, self.half_life)
 
     def get_time(self):
         return self.loop_counter * 1./60
@@ -48,22 +32,80 @@ class SimCLDAControlMultiDispl2D(WindowDispl2D, bmimultitasks.SimCLDAControlMult
     def loop_step(self):
         self.loop_counter += 1
 
-class SimBMIControlMultiDispl2D_PPF(WindowDispl2D, bmimultitasks.SimBMIControlMulti, Autostart):
-    '''
-    Sim of PPF where beta used to generate the spikes is known to the decoder
-    '''
+class PointProcNeuralSim(object):
+    def _init_neural_encoder(self):
+        from riglib.bmi import sim_neurons
+        sim_encoder_fname = os.path.join(os.getenv('HOME'), 'code/bmi3d/tests/ppf', 'sample_spikes_and_kinematics_10000.mat')
+        self.encoder = sim_neurons.load_ppf_encoder_2D_vel_tuning_clda_sim(sim_encoder_fname, dt=1./60) #CosEnc(fname=sim_encoder_fname, return_ts=True)
+
+class PointProcSmoothbatch(object):
+    def create_updater(self):
+        dt = 1./180
+        batch_time = 1./60
+        batch_size = batch_time/dt
+        print batch_size
+        half_life = 120.
+        rho = np.exp(np.log(0.5) / (half_life/batch_time))
+        print rho
+        
+        #self.updater = clda.PPFContinuousBayesianUpdater(self.decoder)
+        self.updater = clda.PPFSmoothbatchSingleThread()
+        self.updater.rho = rho
+        self.updater.batch_time = batch_time
+
+class PointProcContinuous(object):
+    def create_updater(self):
+        self.updater = clda.PPFContinuousBayesianUpdater(self.decoder)
+
+class SimCLDAControlMultiDispl2D(SimTime, WindowDispl2D, bmimultitasks.SimCLDAControlMulti, PointProcNeuralSim, Autostart):
+    update_rate = 0.1
     def __init__(self, *args, **kwargs):
         self.target_radius = 1.8
-        bmimultitasks.SimBMIControlMulti.__init__(self, *args, **kwargs)
-        self.batch_time = 5
+        bmimultitasks.SimCLDAControlMulti.__init__(self, *args, **kwargs)
+        self.batch_time = 10.
         self.half_life  = 20.0
 
-        self.origin_hold_time = 0.250
-        self.terminus_hold_time = 0.250
         self.hdf = SimHDF()
         self.task_data = SimHDF()
-        self.start_time = 0.
-        self.loop_counter = 0
+        SimTime.__init__(self)
+
+    def create_updater(self):
+        clda_input_queue = mp.Queue()
+        clda_output_queue = mp.Queue()
+        #self.updater = clda.KFRML(clda_input_queue, clda_output_queue, self.batch_time, self.half_life)
+        self.updater = clda.KFOrthogonalPlantSmoothbatch(clda_input_queue, clda_output_queue, self.batch_time, self.half_life)
+
+    def create_learner(self):
+        #B = np.mat(np.vstack([0, 0, 0, 1., 1, 1, 0]))
+        #F_target = np.mat(np.hstack([1., 1, 1, 0, 0, 0, 0]))
+        #F_hold = np.mat(np.hstack([0., 0., 0, 0, 0, 0, 0]))
+        #F_dict = dict(hold=F_hold, target=F_target)
+        dt = 0.1
+        A = np.mat([[1., 0, 0, dt, 0, 0, 0], 
+                    [0., 0, 0, 0,  0, 0, 0],
+                    [0., 0, 1, 0, 0, dt, 0],
+                    [0., 0, 0, 0, 0,  0, 0],
+                    [0., 0, 0, 0, 0,  0, 0],
+                    [0., 0, 0, 0, 0,  0, 0],
+                    [0., 0, 0, 0, 0,  0, 1]])
+
+
+        I = np.mat(np.eye(3))
+        B = np.vstack([0*I, I, np.zeros([1,3])])
+        F_target = np.hstack([I, 0*I, np.zeros([3,1])])
+        F_hold = np.hstack([0*I, 0*I, np.zeros([3,1])])
+        F_dict = dict(hold=F_hold, target=F_target)
+        self.learner = clda.OFCLearner(self.batch_size, A, B, F_dict)
+        self.learn_flag = True
+        
+
+#SimTime, WindowDispl2D, bmimultitasks.SimCLDAControlMulti, PointProcNeuralSim, Autostart
+
+class SimCLDAControlMultiDispl2D_PPF(bmimultitasks.CLDAControlPPFContAdapt, SimCLDAControlMultiDispl2D):
+    def __init__(self, *args, **kwargs):
+        super(SimCLDAControlMultiDispl2D_PPF, self).__init__(*args, **kwargs)
+        self.batch_time = 1./10 #60.  # TODO 10 Hz running seems to be hardcoded somewhere
+        self.half_life = 360.
         self.assist_level = 0
 
     def load_decoder(self):
@@ -78,39 +120,36 @@ class SimBMIControlMultiDispl2D_PPF(WindowDispl2D, bmimultitasks.SimBMIControlMu
         beta = data['beta']
         beta = np.vstack([beta[1:, :], beta[0,:]]).T
         beta_dec = riglib.bmi.train.inflate(beta, decoding_states, states, axis=1)
+        beta_dec[:,[3,5]] *= 1
 
         self.decoder = riglib.bmi.train._train_PPFDecoder_sim_known_beta(beta_dec, 
                 self.encoder.units, dt=dt, dist_units='cm')
+        self.decoder.filt.W[3:6, 3:6] = np.eye(3) * 0.4110
         
-    def init(self):
-        '''
-        Instantiate simulation decoder
-        '''
-        bmimultitasks.SimBMIControlMulti.init(self)
-        self.load_decoder()
-
-        self.n_subbins = 3
-        self.last_get_spike_counts_time = 0#-1./60
+    def create_updater(self):
+        #dt = 1./180
+        #batch_time = 1./60
+        #batch_size = batch_time/dt
+        #print batch_size
+        #half_life = 120.
+        #rho = np.exp(np.log(0.5) / (half_life/batch_time))
+        #print rho
+        
+        self.batch_size = 1
+        self.create_learner() # Recreate learner
+        self.updater = clda.PPFContinuousBayesianUpdater(self.decoder)
+        #self.updater = clda.PPFSmoothbatchSingleThread()
+        #self.updater.rho = rho
+        #self.updater.batch_time = batch_time
 
     def _init_neural_encoder(self):
         from riglib.bmi import sim_neurons
         sim_encoder_fname = os.path.join(os.getenv('HOME'), 'code/bmi3d/tests/ppf', 'sample_spikes_and_kinematics_10000.mat')
         self.encoder = sim_neurons.load_ppf_encoder_2D_vel_tuning_clda_sim(sim_encoder_fname, dt=1./60) #CosEnc(fname=sim_encoder_fname, return_ts=True)
-
-    def get_cursor_location(self):
-        spike_counts = self.get_spike_counts()
-        self.call_decoder(spike_counts)
-        return self.decoder['hand_px', 'hand_py', 'hand_pz']
-
-    def get_time(self):
-        return self.loop_counter * 1./60
-
-    def loop_step(self):
-        self.loop_counter += 1
-
+    
 
 gen = genfns.sim_target_seq_generator_multi(8, 1000)
-task = SimBMIControlMultiDispl2D_PPF(gen)
+task = SimCLDAControlMultiDispl2D_PPF(gen)
 #task = SimCLDAControlMultiDispl2D(gen)
 task.init()
 task.run()
