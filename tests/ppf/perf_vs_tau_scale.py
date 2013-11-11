937a707a74a15f0f6641bd26c1212eefe69531d6
nucleus <nucleus@nucleus>
2013-11-11 07:09:37 -0800

    reorganized datasets

--- tests/ppf/perf_vs_tau_scale.py
+++ tests/ppf/perf_vs_tau_scale.py
@@ -5,23 +5,50 @@ import matplotlib.pyplot as plt
 import plotutil
 import numpy as np
 
+reload(performance)
+class TaskEntrySet(object):
+    def __init__(self, blocks):
+        self.task_entries = map(performance._get_te, blocks)
+
+    def map(self, fn):
+        return np.array(map(fn, self.task_entries))
+
+class BMIControlMultiTaskEntrySet(TaskEntrySet):
+    @property
+    def trials_per_min(self):
+        return self.map(lambda te: te.trials_per_min)
+
+    @property
+    def perc_correct(self):
+        return self.map(lambda te: te.perc_correct)
+
+    @property
+    def mean_ME(self):
+        return self.map(lambda te: np.mean(te.ME))
+
+    @property
+    def mean_reach_times(self):
+        return self.map(lambda te: np.mean(te.reach_times()))
+
+        
 fixed_blocks = [2185, 2187, 2189, 2191, 2193, 2195]
-task_entries = map(performance._get_te, fixed_blocks)
-tau_values = map(lambda te: dbfn.TaskEntry(te.decoder_record.entry).params['tau'], task_entries)
-trials_per_min = map(lambda te: te.trials_per_min, task_entries)
-perc_correct = map(lambda te: te.perc_correct, task_entries)
-ME = map(lambda te: np.mean(te.ME), task_entries)
+task_entry_set = BMIControlMultiTaskEntrySet(fixed_blocks)
+
+#task_entries = map(performance._get_te, fixed_blocks)
+#tau_values = map(lambda te: dbfn.TaskEntry(te.decoder_record.entry).params['tau'], task_entries)
+#trials_per_min = map(lambda te: te.trials_per_min, task_entries)
+#perc_correct = map(lambda te: te.perc_correct, task_entries)
+#ME = map(lambda te: np.mean(te.ME), task_entries)
+#reach_times = map(lambda te: np.mean(te.reach_times()), task_entries)
 
 plt.close('all')
 plt.figure()
 axes = plotutil.subplots(3, 1, return_flat=True)
-axes[0].plot(tau_values, trials_per_min)
-axes[1].plot(tau_values, perc_correct)
-axes[2].plot(tau_values, ME)
+axes[0].plot(task_entry_set.trials_per_min)
+axes[1].plot(task_entry_set.perc_correct)
+axes[2].plot(task_entry_set.mean_ME)
+axes[2].plot(task_entry_set.mean_reach_times)
 
-plotutil.set_xlim(axes[0], [min(tau_values), max(tau_values)])
-plotutil.set_xlim(axes[1], [min(tau_values), max(tau_values)])
-plotutil.set_xlim(axes[2], [min(tau_values), max(tau_values)])
 plotutil.set_axlim(axes[0], axis='y')
 plotutil.set_axlim(axes[1], axis='y')
 plotutil.set_axlim(axes[2], axis='y')
