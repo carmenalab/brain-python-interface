115a75a884862aa18b8b381a6a3c3aefef7bd698
preeya <pkhanna104@gmail.com>
2013-11-12 12:14:16 -0800

    added joystick to manualcontrolmultitasks, and added '_train_KFDecoder_cursor_epochs' to train.py (way to train decoders only using select parts of cursor movement on screen)

--- db/namelist.py
+++ db/namelist.py
@@ -82,6 +82,7 @@ tasks = dict(
     ######### V2 tasks
     clda_constrained_sskf_multi = bmimultitasks.CLDAConstrainedSSKFMulti,
     manual_control_multi =manualcontrolmultitasks.ManualControlMulti,
+    joystick_control_multi=manualcontrolmultitasks.JoystickMulti,
     bmi_control_multi = bmimultitasks.BMIControlMulti,
     bmi_manipulated_feedback = bmilowfeedback.BMIControlManipulatedFB,
     clda_control_multi = bmimultitasks.CLDAControlMulti,
@@ -113,6 +114,7 @@ instance_to_model = SubclassDict( {
 
 bmis = dict(
     kalman=bmi.train._train_KFDecoder_manual_control,
+    kalmanCursorEpochs = bmi.train._train_KFDecoder_cursor_epochs,
     kalmanVF=bmi.train._train_KFDecoder_visual_feedback,
     kalmanVFshuf=bmi.train._train_KFDecoder_visual_feedback_shuffled,
     ppfVF=bmi.train._train_PPFDecoder_visual_feedback,
