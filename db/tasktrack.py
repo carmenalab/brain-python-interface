a85c7ce0f459e2a48dbce689753a78312f6a3eda
nucleus <nucleus@nucleus>
2013-11-20 11:23:49 -0800

    commented out the old report dictionary

--- db/tasktrack.py
+++ db/tasktrack.py
@@ -85,10 +85,10 @@ def runtask(cmds, _cmds, websock, **kwargs):
     class NotifyFeat(object):
         def set_state(self, state, *args, **kwargs):
         
-            l = time.time() - self.event_log[0][2] if len(self.event_log) > 0 else 0
-            rep = dict(status=status, State=state or "stopped", length=l)
-            rep.update(report.general(self.__class__, self.event_log, self.reportstats, self.ntrials, self.nrewards, self.reward_len))
-            rep['length'] = time.time() - self.task_start_time
+            #l = time.time() - self.event_log[0][2] if len(self.event_log) > 0 else 0
+            #rep = dict(status=status, State=state or "stopped", length=l)
+            #rep.update(report.general(self.__class__, self.event_log, self.reportstats, self.ntrials, self.nrewards, self.reward_len))
+            #rep['length'] = time.time() - self.task_start_time
 
             self.reportstats['status'] = status
             self.reportstats['State'] = state or 'stopped'
