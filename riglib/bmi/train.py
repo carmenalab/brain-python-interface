c82a7761a0508bb12a2e753cae3b06e7815c3de1
Suraj Gowda <surajgowda33@gmail.com>
2013-11-22 09:49:31 -0800

    moved manual control training down to the bottom of the file

--- riglib/bmi/train.py
+++ riglib/bmi/train.py
@@ -55,147 +55,25 @@ def lookup_cells(cells):
     cells = [ (int(c), ord(u) - 96) for c, u in cellname.findall(cells)]
     return cells
 
-def _train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None], 
-    state_vars=['hand_px', 'hand_py', 'hand_pz', 'hand_vx', 'hand_vy', 'hand_vz', 'offset'], 
-    stochastic_vars=['hand_vx', 'hand_vz', 'offset'], **files):
-    #state_vars=['hand_px', 'hand_pz', 'hand_vx', 'hand_vz', 'offset'], 
-    #stochastic_vars=['hand_vx', 'hand_vz', 'offset'], **files):
-    """
-    Train KFDecoder from manual control
-    """
-    # Open plx file
-    plx = plexfile.openFile(str(files['plexon']))
-    events = plx.events[:].data
-    syskey=0
-    # get system registrations
-    reg = parse.registrations(events)
-    # find the key for the motiontracker system data
-    for key, system in reg.items():
-        if (system == 'otiontracker') or (system == 'motiontracker'): #yes this typo is intentional! we could never figure out why the first character doesn't get recorded in the registration
-            syskey = key
-    # get the corresponding hdf rows
-    rows = parse.rowbyte(events)[syskey][:,0]
-    
-    lower, upper = 0 < rows, rows < rows.max() + 1
-    l, u = tslice
-    if l is not None:
-        lower = l < rows
-    if u is not None:
-        upper = rows < u
-    tmask = np.logical_and(lower, upper)
-    
-    #Trim the mask to have exactly an even multiple of 4 worth of data
-    if sum(tmask) % 4 != 0:
-        midx, = np.nonzero(tmask)
-        tmask[midx[-(len(midx) % 4):]] = False
-    
-    #Grab masked data, remove interpolated data
-    h5 = tables.openFile(files['hdf'])
-    motion = h5.root.motiontracker
-    t, m, d = motion.shape
-    motion = motion[np.nonzero(tmask)[0],:,:].reshape(-1, 4, m, d)#motion = motion[np.tile(tmask, [d,m,1]).T].reshape(-1, 4, m, d)
-    invalid = np.logical_or(motion[...,-1] == 4, motion[...,-1] < 0)
-    motion[invalid] = 0
-    kin = motion.sum(axis=1)
-    
-    # Create PSTH function
-    if cells == None: cells = plx.units
-    units = np.array(cells).astype(np.int32)
-    spike_bin_fn = psth.SpikeBin(units, binlen)
-    
-    neurows = rows[tmask][3::4]
-    neurons = np.array(list(plx.spikes.bin(neurows, spike_bin_fn)))
-    mFR = np.mean(neurons,axis=0)
-    sdFR = np.std(neurons,axis=0)
-    if len(kin) != len(neurons):
-        raise ValueError('Training data and neural data are the wrong length: %d vs. %d'%(len(kin), len(neurons)))
-    
-    # Match kinematics with the task state
-    task_states = ['target', 'hold']
-    states = h5.root.motiontracker_msgs[:]
-    # assign names to each of the states
-    state_numbering = dict((n, i) for i, n in enumerate(np.unique(states['msg'])))
-    
-    # event sequence of interest
-    trial_seq = np.array([state_numbering[n] for n in task_states])
-    idx = np.convolve(trial_seq, trial_seq, 'valid')
-    
-    event_seq = np.array([state_numbering[n] for n in states['msg']])
-    found = np.convolve(event_seq, trial_seq, 'valid') == idx
-    
-    times = states[found]['time']
-    if len(times) % 2 == 1:
-        times = times[:-1]
-    slices = times.reshape(-1, 2)
-    t, m, d = h5.root.motiontracker.shape
-    mask = np.ones((t/4, m, d), dtype=bool)
-    for s, e in slices:
-        mask[s/4:e/4] = False
-    kin = np.ma.array(kin, mask=mask[tmask[3::4]])
-    
-    # calculate velocity
-    kin[(kin[...,:3] == 0).all(-1)] = np.ma.masked
-    kin[kin[...,-1] < 0] = np.ma.masked
-    velocity = np.ma.diff(kin[...,:3], axis=0)*60
-    kin = np.ma.hstack([kin[:-1,:,:3], velocity])
-    
-    hand_kin = kin[:, [14,kin.shape[1]/2+14], :]
-    hand_kin = hand_kin.reshape(len(hand_kin), -1)/10.0 #convert motiontracker kinematic data to cm
-    
-    # train KF model parameters
-    neurons = neurons.T
-    n_neurons = neurons.shape[0]
-    hand_kin = hand_kin.T
-
-    hand_kin_vars = ['hand_px', 'hand_py', 'hand_pz', 'hand_vx', 'hand_vy', 'hand_vz', 'offset']
-
-    train_vars = stochastic_vars[:]
-    try: train_vars.remove('offset')
-    except: pass 
-
-    try:
-        state_inds = [hand_kin_vars.index(x) for x in state_vars]
-        stochastic_inds = [hand_kin_vars.index(x) for x in stochastic_vars]
-    	train_inds = [hand_kin_vars.index(x) for x in train_vars]
-        stochastic_state_inds = [state_vars.index(x) for x in stochastic_vars]
-    except:
-        raise ValueError("Invalid kinematic variable(s) specified for KFDecoder state")
-    C = np.zeros([n_neurons, len(state_inds)])
-    C[:, stochastic_state_inds], Q = kfdecoder.KalmanFilter.MLE_obs_model(hand_kin[train_inds, :], neurons[:,:-1])
-    
-    # State-space model set from expert data
-    A, W = state_space_models.linear_kinarm_kf(update_rate=1./60)
-    A = A[np.ix_(state_inds, state_inds)]
-    W = W[np.ix_(state_inds, state_inds)]
-    
-    # instantiate low-level kf
-    unit_inds, = np.nonzero(np.array(C)[:,-1])
-    ## TODO remove next line and make user option
-    is_stochastic = np.array([False, False, True, True, False])
-    kf = kfdecoder.KalmanFilter(A, W, C[unit_inds,:], Q[np.ix_(unit_inds,unit_inds)], is_stochastic=is_stochastic)
-    units = units[unit_inds,:]
-    mFR = mFR[unit_inds]
-    sdFR = sdFR[unit_inds]
-
-    # instantiate KFdecoder
-    bounding_box = np.array([-250., -140.]), np.array([250., 140.])
-    states_to_bound = ['hand_px', 'hand_pz']
-    decoder = kfdecoder.KFDecoder(kf, mFR, sdFR, units, bounding_box, state_vars, states_to_bound, binlen=binlen)
-    return decoder
-
 def _get_tmask(plx, tslice, syskey_fn=lambda x: x[0] in ['task', 'ask']):
+    '''
+    Find the rows of the plx file to use for training the decoder
+    '''
     # Open plx file
     if isinstance(plx, str) or isinstance(plx, unicode):
         plx = plexfile.openFile(plx)
     events = plx.events[:].data
     syskey=0
+
     # get system registrations
     reg = parse.registrations(events)
+
     # find the key for the motiontracker system data
     for key, system in reg.items():
         if syskey_fn(system):
             syskey = key
             break
+
     # get the corresponding hdf rows
     rows = parse.rowbyte(events)[syskey][:,0]
     
@@ -510,6 +388,134 @@ def shuffle_kf_decoder(decoder):
         # if this decoder never had the RML sufficient statistics
         #   (R, S, T, and ESS) as attributes of decoder.kf
         pass
+    return decoder
+
+def _train_KFDecoder_manual_control(cells=None, binlen=0.1, tslice=[None,None], 
+    state_vars=['hand_px', 'hand_py', 'hand_pz', 'hand_vx', 'hand_vy', 'hand_vz', 'offset'], 
+    stochastic_vars=['hand_vx', 'hand_vz', 'offset'], **files):
+    #state_vars=['hand_px', 'hand_pz', 'hand_vx', 'hand_vz', 'offset'], 
+    #stochastic_vars=['hand_vx', 'hand_vz', 'offset'], **files):
+    """
+    Train KFDecoder from manual control
+    """
+    # Open plx file
+    plx = plexfile.openFile(str(files['plexon']))
+    events = plx.events[:].data
+    syskey=0
+    # get system registrations
+    reg = parse.registrations(events)
+    # find the key for the motiontracker system data
+    for key, system in reg.items():
+        if (system == 'otiontracker') or (system == 'motiontracker'): #yes this typo is intentional! we could never figure out why the first character doesn't get recorded in the registration
+            syskey = key
+    # get the corresponding hdf rows
+    rows = parse.rowbyte(events)[syskey][:,0]
+    
+    lower, upper = 0 < rows, rows < rows.max() + 1
+    l, u = tslice
+    if l is not None:
+        lower = l < rows
+    if u is not None:
+        upper = rows < u
+    tmask = np.logical_and(lower, upper)
+    
+    #Trim the mask to have exactly an even multiple of 4 worth of data
+    if sum(tmask) % 4 != 0:
+        midx, = np.nonzero(tmask)
+        tmask[midx[-(len(midx) % 4):]] = False
+    
+    #Grab masked data, remove interpolated data
+    h5 = tables.openFile(files['hdf'])
+    motion = h5.root.motiontracker
+    t, m, d = motion.shape
+    motion = motion[np.nonzero(tmask)[0],:,:].reshape(-1, 4, m, d)#motion = motion[np.tile(tmask, [d,m,1]).T].reshape(-1, 4, m, d)
+    invalid = np.logical_or(motion[...,-1] == 4, motion[...,-1] < 0)
+    motion[invalid] = 0
+    kin = motion.sum(axis=1)
+    
+    # Create PSTH function
+    if cells == None: cells = plx.units
+    units = np.array(cells).astype(np.int32)
+    spike_bin_fn = psth.SpikeBin(units, binlen)
+    
+    neurows = rows[tmask][3::4]
+    neurons = np.array(list(plx.spikes.bin(neurows, spike_bin_fn)))
+    mFR = np.mean(neurons,axis=0)
+    sdFR = np.std(neurons,axis=0)
+    if len(kin) != len(neurons):
+        raise ValueError('Training data and neural data are the wrong length: %d vs. %d'%(len(kin), len(neurons)))
+    
+    # Match kinematics with the task state
+    task_states = ['target', 'hold']
+    states = h5.root.motiontracker_msgs[:]
+    # assign names to each of the states
+    state_numbering = dict((n, i) for i, n in enumerate(np.unique(states['msg'])))
+    
+    # event sequence of interest
+    trial_seq = np.array([state_numbering[n] for n in task_states])
+    idx = np.convolve(trial_seq, trial_seq, 'valid')
+    
+    event_seq = np.array([state_numbering[n] for n in states['msg']])
+    found = np.convolve(event_seq, trial_seq, 'valid') == idx
+    
+    times = states[found]['time']
+    if len(times) % 2 == 1:
+        times = times[:-1]
+    slices = times.reshape(-1, 2)
+    t, m, d = h5.root.motiontracker.shape
+    mask = np.ones((t/4, m, d), dtype=bool)
+    for s, e in slices:
+        mask[s/4:e/4] = False
+    kin = np.ma.array(kin, mask=mask[tmask[3::4]])
+    
+    # calculate velocity
+    kin[(kin[...,:3] == 0).all(-1)] = np.ma.masked
+    kin[kin[...,-1] < 0] = np.ma.masked
+    velocity = np.ma.diff(kin[...,:3], axis=0)*60
+    kin = np.ma.hstack([kin[:-1,:,:3], velocity])
+    
+    hand_kin = kin[:, [14,kin.shape[1]/2+14], :]
+    hand_kin = hand_kin.reshape(len(hand_kin), -1)/10.0 #convert motiontracker kinematic data to cm
+    
+    # train KF model parameters
+    neurons = neurons.T
+    n_neurons = neurons.shape[0]
+    hand_kin = hand_kin.T
+
+    hand_kin_vars = ['hand_px', 'hand_py', 'hand_pz', 'hand_vx', 'hand_vy', 'hand_vz', 'offset']
+
+    train_vars = stochastic_vars[:]
+    try: train_vars.remove('offset')
+    except: pass 
+
+    try:
+        state_inds = [hand_kin_vars.index(x) for x in state_vars]
+        stochastic_inds = [hand_kin_vars.index(x) for x in stochastic_vars]
+    	train_inds = [hand_kin_vars.index(x) for x in train_vars]
+        stochastic_state_inds = [state_vars.index(x) for x in stochastic_vars]
+    except:
+        raise ValueError("Invalid kinematic variable(s) specified for KFDecoder state")
+    C = np.zeros([n_neurons, len(state_inds)])
+    C[:, stochastic_state_inds], Q = kfdecoder.KalmanFilter.MLE_obs_model(hand_kin[train_inds, :], neurons[:,:-1])
+    
+    # State-space model set from expert data
+    A, W = state_space_models.linear_kinarm_kf(update_rate=1./60)
+    A = A[np.ix_(state_inds, state_inds)]
+    W = W[np.ix_(state_inds, state_inds)]
+    
+    # instantiate low-level kf
+    unit_inds, = np.nonzero(np.array(C)[:,-1])
+    ## TODO remove next line and make user option
+    is_stochastic = np.array([False, False, True, True, False])
+    kf = kfdecoder.KalmanFilter(A, W, C[unit_inds,:], Q[np.ix_(unit_inds,unit_inds)], is_stochastic=is_stochastic)
+    units = units[unit_inds,:]
+    mFR = mFR[unit_inds]
+    sdFR = sdFR[unit_inds]
+
+    # instantiate KFdecoder
+    bounding_box = np.array([-250., -140.]), np.array([250., 140.])
+    states_to_bound = ['hand_px', 'hand_pz']
+    decoder = kfdecoder.KFDecoder(kf, mFR, sdFR, units, bounding_box, state_vars, states_to_bound, binlen=binlen)
     return decoder
 
 def _train_PPFDecoder_2D_sim(stochastic_states, neuron_driving_states, units,
