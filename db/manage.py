09826085d75c98fa5d667b2376af02cadd4b7f53
arc <arc@arc>
2014-01-03 16:32:05 -0800

    manage.py and dbq.py have different behavior depending on which version of django is installed, the version-dependent functionality should now check to see which version of django is in use

--- db/manage.py
+++ db/manage.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 import os
 import sys
-if __name__ == "__main__":
+
+import django
+from distutils.version import LooseVersion, StrictVersion
+
+if StrictVersion('%d.%d.%d' % django.VERSION[0:3]) < StrictVersion('1.6.0'):
+    from django.core.management import execute_manager
+    try:
+        import settings # Assumed to be in the same directory.
+    except ImportError:
+        import sys
+        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+        sys.exit(1)
+    
+    execute_manager(settings)
+else:
     # configure celery
     import celery
     c = celery.Celery()
