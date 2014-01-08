c3f2713f762dbcc2690a4c506c05223937472722
nucleus <nucleus@nucleus>
2014-01-07 18:48:38 -0800

    added python scripts to start and stop drain from server

---
+++ reward_drain_off.py
@@ -0,0 +1,5 @@
+from riglib import reward
+
+r = reward.Basic()
+time.sleep(.5)
+r.drain_off()
---
+++ reward_drain_on.py
@@ -0,0 +1,7 @@
+
+from riglib import reward
+import time
+
+r = reward.Basic()
+time.sleep(.5)
+r.drain(600)
