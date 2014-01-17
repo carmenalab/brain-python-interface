c792242a25250dcb7242bd04732d0fed8b71dca1
nucleus <nucleus@nucleus>
2014-01-16 19:20:06 -0800

    debugging changes to robotarm and added new inverse kinematics

--- db/namelist.py
+++ db/namelist.py
@@ -100,7 +100,7 @@ tasks = dict(
     clda_kf_cg_joint_rml = bmimultitasks.CLDAControlKFCGJoint, 
 )
 
-arms = ['RobotArm2J2D', 'RobotArm2D', 'CursorPlant', 'RobotArm4J2D']
+arms = ['RobotArm2J2D', 'RobotArm2D', 'CursorPlant', 'RobotArmGen2D']
 
 ## BMI seed tasks
 # The below list shows which tasks can be used to train new Decoders
