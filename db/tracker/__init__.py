4850b5eca5e0cd9a7b35e4e9bbbef6a40ec88147
lab <lab@arc.(none)>
2014-03-14 10:16:22 -0700

    If no subjects exist when the server is run, then create the 'Testing' subject

--- db/tracker/__init__.py
+++ db/tracker/__init__.py
@@ -3,6 +3,11 @@ import os
 
 try:
     import models
+    subjects = models.Subject.objects.all()
+    if len(subjects) == 0:
+        subj = models.Subject(name='testing')
+        subj.save()
+
     for m in [models.Task, models.Feature, models.Generator, models.System]:
         m.populate()
 except:
