202c9fe4cf965b10144b50af1b45d2aa281a19be
lab <lab@arc.(none)>
2014-03-21 11:08:24 -0700

    added separate scripts for installing DCHP, graphics on unity 12.04

--- install/install_dhcp.sh
+++ install/install_dhcp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo cp arc_interfaces /etc/network/interfaces
 sudo cp arc_dhcpd.conf /etc/dhcp/dhcpd.conf
+sudo cp arc-isc-dhcp-server /etc/default/isc-dhcp-server
 sudo service isc-dhcp-server restart
