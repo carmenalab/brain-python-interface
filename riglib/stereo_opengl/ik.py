38a1c3e18e967347e003ab61af982d8b16fc34f3
nucleus <nucleus@nucleus>
2014-05-23 11:14:01 -0700

    more cleanup of graphics for N-length kinematic chain

--- riglib/stereo_opengl/ik.py
+++ riglib/stereo_opengl/ik.py
@@ -136,21 +136,27 @@ class RobotArmGen2D(Group):
         # Create links for all but most distal link
         self.links = []
 
-        for i in range(self.num_joints-1):
-            link = Cylinder(radius=self.link_radii[i], height=self.link_lengths[i], color=self.link_colors[i])
+        for i in range(self.num_joints):
             joint = Sphere(radius=self.joint_radii[i], color=self.joint_colors[i])
-            link_i = Group((link, joint))
-            self.links.append(link_i)
+            if i < self.num_joints - 1:
+                link = Cylinder(radius=self.link_radii[i], height=self.link_lengths[i], color=self.link_colors[i])
+                link_i = Group((link, joint))
+                self.links.append(link_i)
+            else:
+                cone = Cone(radius1=self.link_radii[-1], radius2=self.link_radii[-1]/2, height=self.link_lengths[-1], color=self.link_colors[-1])
+                # sphere = Sphere(radius=self.joint_radii[i], color=self.joint_colors[i])
+                distal_link = Group((cone, joint)).translate(0, 0, self.link_lengths[-2])
+                self.links.append(distal_link)                
         # self.links = [Group((Cylinder(radius=self.link_radii[i], height=self.link_lengths[i], color=self.link_colors[i]), Sphere(radius=self.joint_radii[i],color=self.joint_colors[i]))) for i in range(self.num_joints-1)]
         
         # Add distal link as a cone instead of cylinder
-        cone = Cone(radius1=self.link_radii[-1], radius2=self.link_radii[-1]/2, height=self.link_lengths[-1], color=self.link_colors[-1])
-        sphere = Sphere(radius=self.joint_radii[-1], color=self.joint_colors[-1])
-        distal_link = Group((cone, sphere)).translate(0, 0, self.link_lengths[-2])
-        self.links.append(distal_link)
+        # cone = Cone(radius1=self.link_radii[-1], radius2=self.link_radii[-1]/2, height=self.link_lengths[-1], color=self.link_colors[-1])
+        # sphere = Sphere(radius=self.joint_radii[-1], color=self.joint_colors[-1])
+        # distal_link = Group((cone, sphere)).translate(0, 0, self.link_lengths[-2])
+        # self.links.append(distal_link)
         
         self.link_groups = [self.links[-1]]
-        for i in range(1,self.num_joints-1):
+        for i in range(1, self.num_joints-1):
             self.link_groups = self.link_groups + [Group([self.links[-i-1], self.link_groups[i-1]]).translate(0,0,self.link_lengths[-i-2])]
         self.link_groups = self.link_groups + [Group([self.links[0], self.link_groups[2]])]
         self.link_groups.reverse()
