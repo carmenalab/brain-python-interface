3f91bf1389aa8649eab7037f64e252ba48e9b8d1
Suraj Gowda <surajgowda33@gmail.com>
2014-05-26 10:08:57 -0700

    updated install scripts

--- install/install.sh
+++ install/install.sh
@@ -1,14 +1,64 @@
-mkdir -p $HOME/code
-cd $HOME/code
+#!/bin/bash
+####### Declare environment variables
+CODE=$HOME/code/
+BMI3D=$HOME/code/bmi3d ### Directory in which to install the bmi3d software
+
+
+####### Set up directories
+mkdir -p $CODE
+sudo mkdir /backup
+sudo chown $USER /backup
+# make log directory
+mkdir $BMI3D/log
+sudo mkdir /storage
+sudo chown -R lab /storage
+mkdir /storage/plots
+mkdir $CODE/src/
 
 sudo apt-get -y install git gitk
 if [ ! -d "$HOME/code/bmi3d" ]; then
     git clone https://github.com/hgm110/bmi3d.git $HOME/code/bmi3d
 fi
 
+####### Reconfigure Ubuntu package manager
+sudo apt-add-repository "deb http://www.rabbitmq.com/debian/ testing main"
+cd $HOME
+wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+sudo apt-key add rabbitmq-signing-key-public.asc
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+
+## Refresh the package manager's list of available packages
+sudo apt-get update
+
+
+####### Install Ubuntu dependencies
 sudo apt-get -y install python-pip libhdf5-serial-dev
-sudo apt-get -y install python-numpy  # "sudo pip install numpy" didn't work
+sudo apt-get -y install python-numpy
 sudo apt-get -y install python-scipy
+# setup the CIFS 
+sudo apt-get -y install smbclient cifs-utils smbfs
+# matplotlib
+sudo apt-get -y install python-matplotlib
+# pygame
+sudo apt-get -y install mercurial python-dev python-numpy ffmpeg libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+# install tools
+sudo apt-get -y install libtool automake bison flex
+# ssh
+sudo apt-get -y install openssh-server
+# text editors
+sudo apt-get -y install sublime-text vim-gnome
+sudo apt-get -y install rabbitmq-server
+sudo apt-get -y install libusb-dev
+sudo apt-get -y install ipython
+# NIDAQ
+sudo apt-get -y install libcomedi-dev
+sudo apt-get -y install python-comedilib
+sudo apt-get -y install swig
+# DHCP server
+sudo apt-get -y install isc-dhcp-server
+
+
+####### Install Python dependencies
 sudo pip install numexpr 
 sudo pip install cython 
 sudo pip install django-celery 
@@ -22,129 +72,69 @@ sudo pip install pylibftdi
 sudo pip install nitime
 sudo pip install sphinx
 sudo pip install numpydoc
-
-# setup the CIFS 
-sudo apt-get -y install smbclient cifs-utils smbfs
-sudo mkdir /backup
-sudo chown lab /backup
-
-MOUNTSTORAGE="/dev/sdb1 /storage        ext4    defaults 0       0"
-MOUNTBACKUP="//project.eecs.berkeley.edu/carmena /backup cifs noauto,username=sgowda,domain=EECS,sec=ntlmssp,uid=localuser,dir_mode=0777,file_mode=0777 0 0"
-MOUNTPLEXON="//10.0.0.13/PlexonData /storage/plexon  smbfs   user=arc,pass=c@rmena,uid=1000,gid=1000 0 0"
-
-## edit fstab as instructed in the email from EECS
-if [ ! `cat /etc/fstab | tr -s ' ' | cut -d " " -f 2 | grep "/backup"` ]; then
-    sudo sed -i '$a $MOUNTBACKUP' /etc/fstab
-fi
-
-if [ ! `cat /etc/fstab | tr -s ' ' | cut -d " " -f 2 | grep "/storage"` ]; then
-    sudo sed -i '$a $MOUNTSTORAGE' /etc/fstab
-fi
-
-if [ ! `cat /etc/fstab | tr -s ' ' | cut -d " " -f 2 | grep "/storage/plexon"` ]; then
-    sudo sed -i '$a $MOUNTPLEXON' /etc/fstab
-fi
-
-
-# tornado web framework
 sudo pip install tornado
-
-# matplotlib
-sudo apt-get -y install python-matplotlib
-
-# make log directory
-mkdir $HOME/code/bmi3d/log
-
-# Make directory to store plots and other data analysis things
-sudo mkdir /storage
-sudo chown -R lab /storage
-mkdir /storage/plots
-
-# Add the following lines to .bashrc
-sed -i '$a source $HOME/code/bmi3d/pathconfig.sh' $HOME/.bashrc
-
 sudo pip install tables
 
-# Install plexread module
-cd ~/code/bmi3d/riglib
-sudo python setup.py install
 
-# pygame -- copied from http://www.pygame.org/wiki/CompileUbuntu
-# pygame - install dependencies
-
-sudo apt-get -y install mercurial python-dev python-numpy ffmpeg \
-    libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
-    libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
-# pygame - Grab source
-hg clone https://bitbucket.org/pygame/pygame $HOME/pygame
-cd $HOME/pygame
-sudo python setup.py install
-
+####### Download any src code
+git clone https://github.com/sgowda/plot $HOME/code/plotutil
 git clone https://github.com/sgowda/robotics_toolbox $HOME/code/robotics
-sed -i '$a export PYTHONPATH=$PYTHONPATH:$HOME/code/robotics' $HOME/.bashrc
+# pygame
+hg clone https://bitbucket.org/pygame/pygame $HOME/code/pygame
 
-cd $HOME/code/bmi3d/db
-python manage.py syncdb
-# Add superuser 'lab' with password 'lab'
 
-sudo apt-get -y install ipython
+
+
+####### Install source code, configure software
+# plexread module
+cd $BMI3D/riglib
+sudo python setup.py install
+
+# pygame
+cd $HOME/code/pygame
+sudo python setup.py install
+
+# symlink for iPython
 sudo ln -s /usr/bin/ipython /usr/bin/ipy
 
 # NIDAQ software
-sudo apt-get -y install libcomedi-dev
-sudo apt-get -y install python-comedilib
-sudo apt-get -y install swig
 $HOME/code/bmi3d/riglib/nidaq/build.sh
-sudo cp $HOME/code/bmi3d/install/udev/comedi.rules /etc/udev/rules.d/
-sudo chmod a+r /etc/udev/rules.d/comedi.rules 
-sudo udevadm control --reload-rules
-# Add 'lab' user to the iocard group to give write access to the NIDAQ card
-sudo usermod -a -G iocard lab
-# Give 'lab' user access to the dialout group to connect to serial ports
-sudo usermod -a -G dialout lab
 
-# RabbitMQ
-sudo apt-add-repository "deb http://www.rabbitmq.com/debian/ testing main"
-# sudo sed -i '$a deb http://www.rabbitmq.com/debian/ testing main' /etc/apt/sources.list
-cd $HOME
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo apt-key add rabbitmq-signing-key-public.asc
-sudo apt-get -y install rabbitmq-server
-
-# DHCP for neural recording platform
-sudo apt-get -y install isc-dhcp-server
-## Add 'eth1' to interfaces in /etc/default/isc-dhcp-server
-# Edit /etc/dhcp/dhcpd.conf
-sudo /etc/init.d/isc-dhcp-server restart
-
-
-sudo apt-get -y install libusb-dev
-mkdir $HOME/src/
-cd $HOME/src/
+# Phidgets libraries
+cd $CODE/src/
 wget http://www.phidgets.com/downloads/libraries/libphidget.tar.gz
 tar xzf libphidget.tar.gz 
 cd libphidget*
 ./configure
 make
 sudo make install
-# Copy udev rules
-sudo cp udev/99-phidgets.rules /etc/udev/rules.d
 
-cd $HOME/src/
+cd $CODE/src/
 wget http://www.phidgets.com/downloads/libraries/PhidgetsPython.zip
 unzip PhidgetsPython.zip  
 cd PhidgetsPython
 sudo python setup.py install
 
-# Graphics driver stuff 
 
-sudo apt-get -y install libtool automake bison flex
-sudo apt-get -y install openssh-server
-git clone https://github.com/sgowda/plot $HOME/code/plotutil
 
-sudo apt-get install apt-show-versions
+####### Configure udev rules, permissions
+# Phidgets
+sudo cp udev/99-phidgets.rules /etc/udev/rules.d
+sudo chmod a+r /etc/udev/rules.d/99-phidgets.rules
+# NIDAQ
+sudo cp $HOME/code/bmi3d/install/udev/comedi.rules /etc/udev/rules.d/
+sudo chmod a+r /etc/udev/rules.d/comedi.rules 
+sudo udevadm control --reload-rules
+# Group permissions
+sudo usermod -a -G iocard lab # NIDAQ card belongs to iocard group
+sudo usermod -a -G dialout lab # Serial ports belong to 'dialout' group
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo apt-get update
-sudo apt-get -y install sublime-text
 
+####### Reconfigure .bashrc
+sed -i '$a export PYTHONPATH=$PYTHONPATH:$HOME/code/robotics' $HOME/.bashrc
+sed -i '$a source $HOME/code/bmi3d/pathconfig.sh' $HOME/.bashrc
+source $HOME/.bashrc
+
+cd $HOME/code/bmi3d/db
+python manage.py syncdb
+# Add superuser 'lab' with password 'lab'
