d39171257c14255f43d6ed6150ef64cb4caadcab
lab <lab@arc.(none)>
2014-06-09 12:47:29 -0700

    added reward feature compatibile with KINARM rig

--- riglib/experiment/features.py
+++ riglib/experiment/features.py
@@ -15,6 +15,11 @@ from riglib.bmi import extractor
 
 from . import traits, experiment
 
+
+from riglib.nidaq import pcidio
+import comedi
+import time
+
 class RewardSystem(traits.HasTraits):
     '''Use the reward system during the reward phase'''
     def __init__(self, *args, **kwargs):
@@ -27,6 +32,39 @@ class RewardSystem(traits.HasTraits):
             self.reward.reward(self.reward_time*1000.)
             self.reportstats['Reward #'] = self.reportstats['Reward #'] + 1
         super(RewardSystem, self)._start_reward()
+
+class TTLReward(traits.HasTraits):
+    def __init__(self, *args, **kwargs):
+        self.com = comedi.comedi_open('/dev/comedi0')
+        super(TTLReward, self).__init__(*args, **kwargs)        
+
+# // extern int ttl_reward_pulse(float pulse_time) {
+# //     if (ni == NULL) {
+# //         init("/dev/comedi0");
+# //     }
+# //     uint val = 0x800000; //0x900000; //16777215;
+# //     uint subdevice = 0;
+# //     uint write_mask = 0x800000;
+# //     uint base_channel = 0;
+# //     comedi_dio_bitfield2(ni, subdevice, write_mask, &val, base_channel);
+
+# //     sleep(pulse_time);
+
+# //     uint val2 = 0;
+# //     comedi_dio_bitfield2(ni, subdevice, write_mask, &val2, base_channel);
+
+
+    def _start_reward(self):
+        subdevice = 0
+        write_mask = 0x800000
+        val = 0x800000
+        base_channel = 0
+        comedi.comedi_dio_bitfield2(self.com, subdevice, write_mask, val, base_channel)
+        time.sleep(self.reward_time)
+        comedi.comedi_dio_bitfield2(self.com, subdevice, write_mask, 0x000000, base_channel)
+        super(RewardSystem, self)._start_reward()
+        
+
 
 class Autostart(traits.HasTraits):
     '''Automatically begins the trial from the wait state, with a random interval drawn from `rand_start`'''
