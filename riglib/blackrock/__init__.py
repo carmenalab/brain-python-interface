711064f9a339bb05c5066f621acdbb389d2dc8e2
Siddharth Dangi <siddharthdangi@gmail.com>
2014-06-20 14:01:02 -0700

    cleaned up soe code in blackrock/__init__.py

--- riglib/blackrock/__init__.py
+++ riglib/blackrock/__init__.py
@@ -50,9 +50,7 @@ class LFP(object):
         return (d.chan, d.samples)
 
 
-# old
-# use this class if using only one data source for armassist+rehand
-# for use with a MultiChanDataSource
+# old -- was using before when there was only one MCDS for armassist+rehand
 # class FeedbackData(object):
 #     update_freq = 25.  # every 40 ms -- TODO check
 
@@ -74,58 +72,49 @@ class LFP(object):
 #         return (d.state_name, np.array([d.value]))
 
 
-# new
-# use these 2 classes if using separate data sources for armassist and rehand
+class FeedbackData(object):
+    '''Parent class for ArmAssistData and ReHandData below.'''
 
-# TODO -- make a common parent class for these two
+    def __init__(self, client_cls):
+        self.client = client_cls()
+
+    def start(self):
+        self.client.start()
+        self.data = self.client.get_feedback_data()
+
+    def stop(self):
+        self.client.stop()
+
+    def get(self):
+        d = self.data.next()
+
+        return np.array([(tuple(d.data), tuple(d.ts), d.arrival_ts)], dtype=self.dtype)
+
+    @staticmethod
+    def _get_dtype(state_names):
+        sub_dtype_data = np.dtype([(state_name, np.float64) for state_name in state_names])
+        sub_dtype_ts   = np.dtype([(state_name, np.int64) for state_name in state_names])
+        return np.dtype([('data', sub_dtype_data), ('ts', sub_dtype_ts), ('arrival_ts', np.float64)])
+
 
 # for use with a DataSource
-class ArmAssistData(object):
+class ArmAssistData(FeedbackData):
     update_freq = 25.  # every 40 ms -- TODO check
 
     state_names = ['aa_px', 'aa_py', 'aa_ppsi', 'aa_vx', 'aa_vy', 'aa_vpsi']
-    sub_dtype_data = np.dtype([(state_name, np.float64) for state_name in state_names])
-    sub_dtype_ts   = np.dtype([(state_name, np.int64) for state_name in state_names])
-    dtype = np.dtype([('data', sub_dtype_data), ('ts', sub_dtype_ts), ('arrival_ts', np.float64)])
+    dtype = FeedbackData._get_dtype(state_names)
 
     def __init__(self):
-        self.client = udp_feedback_client.ArmAssistClient()
-
-    def start(self):
-        self.client.start()
-        self.data = self.client.get_feedback_data()
-
-    def stop(self):
-        self.client.stop()
-
-    def get(self):
-        d = self.data.next()
-
-        return np.array([(tuple(d.data), tuple(d.ts), d.arrival_ts)], dtype=self.dtype)
-
+        super(ArmAssistData, self).__init__(udp_feedback_client.ArmAssistClient)
 
 
 # for use with a DataSource
-class ReHandData(object):
+class ReHandData(FeedbackData):
     update_freq = 25.  # every 40 ms -- TODO check
 
     state_names = ['rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono', 'rh_vthumb', 'rh_vindex', 'rh_vfing3', 'rh_vprono']
-    sub_dtype_data = np.dtype([(state_name, np.float64) for state_name in state_names])
-    sub_dtype_ts   = np.dtype([(state_name, np.int64) for state_name in state_names])
-    dtype = np.dtype([('data', sub_dtype_data), ('ts', sub_dtype_ts), ('arrival_ts', np.float64)])
+    dtype = FeedbackData._get_dtype(state_names)
 
     def __init__(self):
-        self.client = udp_feedback_client.ReHandClient()
-
-    def start(self):
-        self.client.start()
-        self.data = self.client.get_feedback_data()
-
-    def stop(self):
-        self.client.stop()
-
-    def get(self):
-        d = self.data.next()
-
-        return np.array([(tuple(d.data), tuple(d.ts), d.arrival_ts)], dtype=self.dtype)
+        super(ReHandData, self).__init__(udp_feedback_client.ReHandClient)
 
