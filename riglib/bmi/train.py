7d609a5f3586ec8b32690b6186d6b3224324c07f
Siddharth Dangi <siddharthdangi@gmail.com>
2014-07-02 17:53:59 +0200

    implemented _get_tmask_blackrock function in train.py

--- riglib/bmi/train.py
+++ riglib/bmi/train.py
@@ -301,10 +301,56 @@ def _get_tmask(plx, tslice, syskey_fn=lambda x: x[0] in ['task', 'ask'], sys_nam
     return tmask, rows
 
 
-def _get_tmask_blackrock(nevfile, tslice, syskey_fn=lambda x: x[0] in ['task', 'ask'], sys_name='task'):
+def _get_tmask_blackrock(nev_fname, tslice, syskey_fn=lambda x: x[0] in ['task', 'ask'], sys_name='task'):
     ''' Find the rows of the nev file to use for training the decoder.'''
 
-    raise NotImplementedError
+    nev_hdf_fname = nev_fname + '.hdf'
+    if not os.path.isfile(nev_hdf_fname):
+        # convert .nev file to hdf file using Blackrock's n2h5 utility
+        subprocess.call(['n2h5', nev_fname, nev_hdf_fname])
+
+    import h5py
+    nev_hdf = h5py.File(nev_hdf_fname, 'r')
+
+    path = 'channel/digital00001/digital_set'
+    ts = nev_hdf.get(path).value['TimeStamp']
+    msgs = nev_hdf.get(path).value['Value']
+
+    # copied from riglib/nidaq/parse.py
+    msgtype_mask = 0b0000111<<8
+    auxdata_mask = 0b1111000<<8
+    rawdata_mask = 0b11111111
+    msgtype = np.right_shift(np.bitwise_and(msgs, msgtype_mask), 8).astype(np.uint8)
+    auxdata = np.right_shift(np.bitwise_and(msgs, auxdata_mask), 8).astype(np.uint8)
+    rawdata = np.bitwise_and(msgs, rawdata_mask)
+
+    # data is an N x 4 matrix that will be the argument to parse.registrations()
+    data = np.vstack([ts, msgtype, auxdata, rawdata]).T
+
+    # get system registrations
+    reg = parse.registrations(data)
+
+    syskey = None
+
+    for key, system in reg.items():
+        if sys_eq(system[0], sys_name):
+            syskey = key
+            break
+
+    if syskey is None:
+        raise Exception('No source registration saved in the file!')
+
+    # get the corresponding hdf rows
+    rows = parse.rowbyte(data)[syskey][:,0]
+    
+    lower, upper = 0 < rows, rows < rows.max() + 1
+    l, u = tslice
+    if l is not None:
+        lower = l < rows
+    if u is not None:
+        upper = rows < u
+    tmask = np.logical_and(lower, upper)
+    return tmask, rows
     
 
 
