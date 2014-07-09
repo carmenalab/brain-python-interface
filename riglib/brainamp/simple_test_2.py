5cc513870c8dd23bab25a947a00e539dbfc97630
Siddharth Dangi <siddharthdangi@gmail.com>
2014-07-09 22:49:43 +0200

    moved some test files to tests/ibmi

--- riglib/brainamp/simple_test_2.py
+++
@@ -1,9 +0,0 @@
-import time
-from riglib import source, brainamp
-
-
-from riglib.experiment import traits  # TODO -- not need, remove
-
-channels = [1]
-s = source.MultiChanDataSource(brainamp.EMG, channels=channels)
-s.start()
