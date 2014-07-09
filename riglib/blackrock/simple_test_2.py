36cf6da6169bacefb5c89eabd171773b75b7b97f
Siddharth Dangi <siddharthdangi@gmail.com>
2014-07-09 22:38:23 +0200

    moved some test files around

--- riglib/blackrock/simple_test_2.py
+++
@@ -1,9 +0,0 @@
-import time
-from riglib import source, blackrock
-
-
-from riglib.experiment import traits  # TODO -- not need, remove
-
-channels = [5, 6, 7, 8]
-s = source.DataSource(blackrock.Spikes, channels=channels)
-s.start()
