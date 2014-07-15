8adc236caa1d5962380e16e2b75263c4644de268
Helene Moorman <helenem@HMSDH.(none)>
2014-07-15 15:07:26 -0700

    not sure

--- db/paths.py
+++ db/paths.py
@@ -1,2 +1,4 @@
-data_path = '/storage/'
-rawdata_path = '/storage/rawdata/'
+data_path = '/storage/bmi3d/'
+rawdata_path = '/storage/bmi3d/rawdata/'
+
+pathdict = {'/home/helenem/code/bmi3d/db/db.sql':'/storage/bmi3d/', '/home/helenem/code/bmi3d/db/db_exorig.sql':'/storage/exorig/'}
