updated ibmi state spaces to have correct workspace bounds (42x30); working LFC assist (weighted average method) for ArmAssist; working sim task when CLDA turns off (sim ArmAssist goes to targets by itself with both non-LFC and LFC assisters)

riglib/bmi/bmi.py
@@ -459,18 +459,26 @@ class Decoder(object):
         # re-format as a column matrix
         neural_obs = np.mat(neural_obs.reshape(-1,1))
 
+        weighted_avg_lfc = kwargs.pop('weighted_avg_lfc', False)
+        x = self.filt.state.mean
+        
         # Run the filter
         # self.filt(neural_obs, Bu=Bu)
-        filt_kwargs = dict()
-        if 'obs_is_control_independent' in kwargs:
-            filt_kwargs['obs_is_control_independent'] = kwargs['obs_is_control_independent']
+        filt_kwargs = dict(obs_is_control_independent=weighted_avg_lfc)        
+        
         self.filt(neural_obs, Bu=Bu, **filt_kwargs)
 
-        if assist_level > 0:
-            self.filt.state.mean = (1-assist_level)*self.filt.state.mean + assist_level*Bu
+        # if assist_level > 0:
+        #     self.filt.state.mean = (1-assist_level)*self.filt.state.mean + assist_level*Bu
 
-        # print 'before bounder:'
-        # print self.filt.state.mean
+        if assist_level > 0:
+            if weighted_avg_lfc:
+                # calculates assist as:
+                #   (1-assist)*(Ax + K(y-CAx)) + assist*(Ax + Bu)
+                # Note: the variable "Bu" here is really equal to assist_level*Bu
+                self.filt.state.mean = (1-assist_level)*self.filt.state.mean + assist_level*self.filt.A*x + Bu
+            else:
+                self.filt.state.mean = (1-assist_level)*self.filt.state.mean + assist_level*Bu
 
         # Bound cursor, if any hard bounds for states are applied
         if hasattr(self, 'bounder'):

riglib/ismore/plants.py
@@ -195,12 +195,12 @@ class ArmAssistPlantNoUDP(object):
         self.aa.daemon = True
 
 
-    # # a "magic" function that instantaneously moves the ArmAssist and ReHand to a new configuration
-    # # IMPORTANT: only use to set initial position/orientation
-    # def set_pos(self, pos):
-    #     '''Magically set position (x, y, psi) in units of (cm, cm, rad).'''
-    #     wf = np.mat(pos).T
-    #     self.aa._set_wf(wf)
+    # a "magic" function that instantaneously moves the ArmAssist to a new configuration
+    # IMPORTANT: only use to set initial position/orientation
+    def set_pos(self, pos):
+        '''Magically set position (x, y, psi) in units of (cm, cm, rad).'''
+        wf = np.mat(pos).T
+        self.aa._set_wf(wf)
 
     def init(self):
         pass