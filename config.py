4b322b4e8fc457c5359b0409b1389468ad6f50bb
nucleus <nucleus@nucleus>
2014-08-18 08:09:36 -0700

    extracting display_start_pos and reward_system_version from config file

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ parser = SafeConfigParser()
 parser.read(os.path.expandvars('$BMI3D/config'))
 
 recording_system = dict(parser.items('recording_sys'))['make']
+
+window_start_x = dict(parser.items('graphics'))['window_start_x']
+window_start_y = dict(parser.items('graphics'))['window_start_y']
+display_start_pos = '%s,%s' % (window_start_x, window_start_y)
+
+reward_system_version = int(dict(parser.items('reward_sys'))['version'])
