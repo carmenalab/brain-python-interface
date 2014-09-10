32f63079bf3059876f38ad6ca3cdaebfe2a65a24
Arc Reactor <arc@arc>
2014-09-10 15:06:43 -0700

    added the 'files' field back to the web interface

--- db/html/static/resources/js/list.js
+++ db/html/static/resources/js/list.js
@@ -269,7 +269,7 @@ TaskEntry.prototype.update = function(info) {
 	}
 
 	if (numfiles > 0) {
-		// $("#files").append(this.filelist).show();
+		$("#files").append(this.filelist).show();
 
 		// make the BMI show up if there's a plexon file in the provided data files
 		var found = false;
@@ -330,7 +330,7 @@ TaskEntry.prototype.destroy = function() {
 
     // Hide the 'files' field
 	$("#files").hide();
-	// $(this.filelist).remove();
+	$(this.filelist).remove();
 
 	if (this.idx != null) {
 		var idx = "row"+this.idx;
