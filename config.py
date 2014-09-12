1adc219fbd0aba3ee10475bd5e76a19b7fce1c06
Suraj Gowda <surajgowda33@gmail.com>
2014-09-12 10:20:00 -0700

    config file now in the new more generic format

--- config.py
+++ config.py
@@ -1,18 +1,31 @@
 from ConfigParser import SafeConfigParser
 import os
+import sys
 
-parser = SafeConfigParser()
-parser.read(os.path.expandvars('$BMI3D/config'))
+## Hack getattr: http://stackoverflow.com/questions/2447353/getattr-on-a-module
+class Config:
+	def __init__(self):
+		parser = SafeConfigParser()
+		self.parser = parser
+		self.parser.read(os.path.expandvars('$BMI3D/config'))
 
-recording_system = dict(parser.items('recording_sys'))['make']
-data_path = dict(parser.items('db_config_default'))['data_path']
+		self.recording_system = dict(parser.items('recording_sys'))['make']
+		self.data_path = dict(parser.items('db_config_default'))['data_path']
 
-db_config_default = dict(parser.items('db_config_default'))
-db_config_bmi3d = dict(parser.items('db_config_bmi3d'))
-db_config_exorig = dict(parser.items('db_config_exorig'))
+		self.db_config_default = dict(parser.items('db_config_default'))
+		self.db_config_bmi3d = dict(parser.items('db_config_bmi3d'))
+		self.db_config_exorig = dict(parser.items('db_config_exorig'))
 
-window_start_x = dict(parser.items('graphics'))['window_start_x']
-window_start_y = dict(parser.items('graphics'))['window_start_y']
-display_start_pos = '%s,%s' % (window_start_x, window_start_y)
+		self.window_start_x = dict(parser.items('graphics'))['window_start_x']
+		self.window_start_y = dict(parser.items('graphics'))['window_start_y']
+		self.display_start_pos = '%s,%s' % (self.window_start_x, self.window_start_y)
 
-reward_system_version = int(dict(parser.items('reward_sys'))['version'])
+		self.reward_system_version = int(dict(parser.items('reward_sys'))['version'])		
+
+	def __getattr__(self, attr):
+		if attr in self.parser.sections():
+			return dict(self.parser.items(attr))
+		else:
+			return super(Hack, self).__getattr__(attr)
+
+config = Config()
