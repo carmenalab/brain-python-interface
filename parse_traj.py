5ab8b676ca8b6ae2ef2b6ed1fb5da97299064fef
Siddharth Dangi <siddharthdangi@gmail.com>
2014-09-30 14:22:23 -0700

    cleaned up code and added some comments

--- parse_traj.py
+++ parse_traj.py
@@ -7,14 +7,18 @@ import pickle
 from utils.constants import *
 
 
-hdf = tables.openFile('/storage/rawdata/hdf/test20140930_02.hdf')
-pkl_name = 'traj_psi.pkl'
+hdf_name = '/storage/rawdata/hdf/test20140930_02.hdf'
+pkl_name = 'traj_playback.pkl'
 
+
+aa_pos_states = ['aa_px', 'aa_py', 'aa_ppsi']
+rh_pos_states = ['rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono']
+
+# load task, armassist, and rehand data from hdf file
+hdf = tables.openFile(hdf_name)
 task      = hdf.root.task
 task_msgs = hdf.root.task_msgs
-
 armassist = hdf.root.armassist
-
 try:
     rehand = hdf.root.rehand
 except:
@@ -23,46 +27,38 @@ except:
 else:
     rehand_flag = True
 
-
+# create a dictionary of trajectories, indexed by trial_type
 traj = dict()
 
 trial_idxs = [idx for (idx, msg) in enumerate(task_msgs[:]['msg']) if msg == 'trial']
-wait_idxs  = [idx for (idx, msg) in enumerate(task_msgs[:]['msg']) if msg == 'wait']
-
 for idx in trial_idxs:
     t_start = task_msgs[idx]['time']
-
     trial_type = task[t_start]['trial_type']
-    print trial_type
-    
-    if not (trial_type in traj):
+
+    # if we haven't already saved a trajectory for this trial_type
+    if trial_type not in traj:
         traj[trial_type] = dict()
 
-        t_end = task_msgs[idx+1]['time'] - 1
+        t_end    = task_msgs[idx+1]['time'] - 1
         ts_start = task[t_start]['ts'][0]
         ts_end   = task[t_end]['ts'][0]
+        traj[trial_type]['ts_start'] = ts_start
+        traj[trial_type]['ts_end']   = ts_end
 
-        traj[trial_type]['ts_start']  = ts_start
-        traj[trial_type]['ts_end']    = ts_end
-
-        idxs = [i for (i, x) in enumerate(armassist[:]) if ts_start <= x['ts_arrival']/1e6 <= ts_end]
-
-        df1 = pd.DataFrame(np.array(armassist[idxs]['data'].tolist()).T, index=['aa_px', 'aa_py', 'aa_ppsi'])
-        aa_px_ts = (np.array(armassist[idxs]['ts'].tolist()).T)[0:1, :]
-        df2 = pd.DataFrame(aa_px_ts, index=['ts'])
-        df_aa = pd.concat([df1, df2])
-        traj[trial_type]['armassist'] = df_aa
-
-        if rehand_flag:
-            df_rh = pd.DataFrame(np.array(rehand[idxs]['data'].tolist()), index=['rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono'])
-            traj[trial_type]['rehand']  = df_rh 
-
+        # save task data
         idxs = [i for i in range(len(task[:])) if t_start <= i <= t_end]
-
         traj[trial_type]['task'] = task[idxs]
-        try:
-            traj[trial_type]['task'] = task[idxs]
-        except:
-            pass
+
+        # save armassist data
+        idxs = [i for (i, x) in enumerate(armassist[:]) if ts_start <= us_to_s*x['ts_arrival'] <= ts_end]
+        df_aa1 = pd.DataFrame(np.array(armassist[idxs]['data'].tolist()).T, index=aa_pos_states)
+        aa_px_ts = (np.array(armassist[idxs]['ts'].tolist()).T)[0:1, :]
+        df_aa2 = pd.DataFrame(aa_px_ts, index=['ts'])
+        traj[trial_type]['armassist'] = pd.concat([df_aa1, df_aa2])
+
+        # save rehand data
+        if rehand_flag:
+            idxs = [i for (i, x) in enumerate(rehand[:]) if ts_start <= us_to_s*x['ts_arrival'] <= ts_end]
+            traj[trial_type]['rehand'] = pd.DataFrame(np.array(rehand[idxs]['data'].tolist()).T, index=rh_pos_states)
 
 pickle.dump(traj, open(pkl_name, 'wb'))
--- sample_traj.py
+++ sample_traj.py
@@ -7,8 +7,9 @@ import pickle
 from utils.constants import *
 
 
-pkl_name = 'trajectories.pkl'
-filename = '/home/lab/Desktop/Kinematic data ArmAssist/Epoched data/epoched_kin_data/NI_sess05_20140610/NI_B1S005R01.mat'
+pkl_name = 'traj_reference.pkl'
+mat_name = '/home/lab/Desktop/Kinematic data ArmAssist/Epoched data/epoched_kin_data/NI_sess05_20140610/NI_B1S005R01.mat'
+
 
 index = [
     'Time',
@@ -37,47 +38,49 @@ field_mapping = {
     'Fingers(dg)':      'rh_pfing3',
 }
 
-
-
+# ArmAssist and ReHand trajectories are saved as separate pandas DataFrames with the following indexes
 aa_fields = ['ts', 'aa_px', 'aa_py', 'aa_ppsi']
-rh_fields = ['ts', 'rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono']  # TODO -- what about rh velocity? not recorded during these experiments?
+rh_fields = ['ts', 'rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono']  # TODO -- what about rh velocity?
+
 
 def preprocess_data(df):
+    # rename dataframe fields to match state space names used in Python code
     df = df.rename(field_mapping)
 
+    # convert units to usec, cm, rad
     df.ix['ts'] *= ms_to_us
     df.ix[['aa_px', 'aa_py']] *= mm_to_cm
     df.ix[['aa_ppsi', 'rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono']] *= deg_to_rad
     
+    # translate trajectories have a particular starting position
     starting_pos = np.array([21., 15.])
     pos_offset = df.ix[['aa_px', 'aa_py'], 0] - starting_pos
-
     df.ix['aa_px', :] -= pos_offset[0]
     df.ix['aa_py', :] -= pos_offset[1]
 
     return df
 
 
-mat = sio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+# load kinematic data
+mat = sio.loadmat(mat_name, struct_as_record=False, squeeze_me=True)
 kin_epoched = mat['kin_epoched']
 
+# create a dictionary of trajectories, indexed by trial_type
 traj = dict()
 for i, kin in enumerate(kin_epoched):
     df = preprocess_data(pd.DataFrame(kin.T, index=index))
 
+    # assign an arbitrary trial_type name
     if i % 2 == 0:
         trial_type = 'touch red'
     else:
         trial_type = 'pinch grip green'
 
-    if not (trial_type in traj):
+    # if we haven't already saved a trajectory for this trial_type
+    if trial_type not in traj:
         traj[trial_type] = dict()
-        
-        ts_start = df.ix['ts', 0]
-        ts_end   = df.ix['ts', df.columns[-1]]
-
-        traj[trial_type]['ts_start']  = ts_start
-        traj[trial_type]['ts_end']    = ts_end
+        traj[trial_type]['ts_start']  = df.ix['ts', 0]
+        traj[trial_type]['ts_end']    = df.ix['ts', df.columns[-1]]
         traj[trial_type]['armassist'] = df.ix[aa_fields, :]
         traj[trial_type]['rehand']    = df.ix[rh_fields, :]
 
