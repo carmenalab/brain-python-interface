f1774272c41b91039719acd3ba741b2808dacf55
Siddharth Dangi <siddharthdangi@gmail.com>
2014-10-28 19:58:10 +0100

    added simple code for bounding command velocity to PlaybackTrajectories task

--- riglib/ismore/__init__.py
+++ riglib/ismore/__init__.py
@@ -20,27 +20,13 @@ class FeedbackData(object):
     def stop(self):
         self.client.stop()
 
-    # def get(self):
-    #     d = self.data.next()
-    #     return np.array([(tuple(d.data), tuple(d.ts), d.ts_sent, d.ts_arrival, d.freq)], dtype=self.dtype)
-
-    # @staticmethod
-    # def _get_dtype(state_names):
-    #     sub_dtype_data = np.dtype([(name, np.float64) for name in state_names])
-    #     sub_dtype_ts   = np.dtype([(name, np.int64)   for name in state_names])
-    #     return np.dtype([('data',       sub_dtype_data),
-    #                      ('ts',         sub_dtype_ts),
-    #                      ('ts_sent',    np.float64),
-    #                      ('ts_arrival', np.float64),
-    #                      ('freq',       np.float64)])
-
 
 class ArmAssistData(FeedbackData):
     '''For use with a DataSource in order to acquire feedback data from the 
     ArmAssist application.
     '''
 
-    update_freq = 15.  # TODO check
+    update_freq = 15.
     client_cls = udp_feedback_client.ArmAssistClient
 
     state_names = ['aa_px', 'aa_py', 'aa_ppsi']
@@ -72,7 +58,7 @@ class ReHandData(FeedbackData):
     ReHand application.
     '''
 
-    update_freq = 200.  # TODO check
+    update_freq = 200.
     client_cls = udp_feedback_client.ReHandClient
 
     state_names = ['rh_pthumb', 'rh_pindex', 'rh_pfing3', 'rh_pprono', 
