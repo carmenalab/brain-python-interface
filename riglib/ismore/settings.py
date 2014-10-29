b9b3de4f2248c9c9fa4d8bd34404e8e3f990f16a
Siddharth Dangi <siddharthdangi@gmail.com>
2014-10-29 02:32:54 +0100

    redefined starting_pos so that it doesn't initialize from a dict (otherwise the .values attribute might not be in order)

--- riglib/ismore/settings.py
+++ riglib/ismore/settings.py
@@ -11,12 +11,22 @@ armassist_udp_client = ('127.0.0.1', 5002)
 # rehand_udp_client    = ('127.0.0.1', 5003)
 rehand_udp_client    = ('192.168.137.2', 5003)
 
-starting_pos = pd.Series({
-    'aa_px':     43,                # cm
-    'aa_py':     18.,               # cm
-    'aa_ppsi':    0.,               # rad
-    'rh_pthumb': 30. * deg_to_rad,  # rad
-    'rh_pindex': 30. * deg_to_rad,  # rad
-    'rh_pfing3': 30. * deg_to_rad,  # rad
-    'rh_pprono':  2. * deg_to_rad,  # rad
-})
+pos_states = [
+    'aa_px',
+    'aa_py',
+    'aa_ppsi',
+    'rh_pthumb',
+    'rh_pindex',
+    'rh_pfing3',
+    'rh_pprono',
+]
+
+starting_pos = pd.Series(0.0, pos_states)
+starting_pos['aa_px']     = 43.               # cm
+starting_pos['aa_py']     = 18.               # cm
+starting_pos['aa_ppsi']   =  0.               # rad
+starting_pos['rh_pthumb'] = 30. * deg_to_rad  # rad
+starting_pos['rh_pindex'] = 30. * deg_to_rad  # rad
+starting_pos['rh_pfing3'] = 30. * deg_to_rad  # rad
+starting_pos['rh_pprono'] =  2. * deg_to_rad  # rad
+
