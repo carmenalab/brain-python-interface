94bb3d4db7726a847526b6d87ba8082843ce5b91
srsummerson <srsummerson@gmail.com>
2014-11-06 20:45:28 -0800

    working on stimulus pulse file

--- riglib/stimulus_pulse.py
+++ riglib/stimulus_pulse.py
@@ -7,14 +7,15 @@ import comedi
 
 class stimulus_pulse(object):
 	com = comedi.comedi_open('/dev/comedi0')
-	def __init__(self):
+	
+	def __init__(self, *args, **kwargs):
 		#self.com = comedi.comedi_open('/dev/comedi0')
-        super(TTLStimulation, self).__init__(*args, **kwargs)
-        subdevice = 0
-        write_mask = 0x800000
-        val = 0x000000
-        base_channel = 0
-        comedi.comedi_dio_bitfield2(self.com, subdevice, write_mask, val, base_channel)
+		super(stimulus_pulse, self).__init__(*args, **kwargs)
+		subdevice = 0
+		write_mask = 0x800000
+		val = 0x000000
+		base_channel = 0
+		comedi.comedi_dio_bitfield2(self.com, subdevice, write_mask, val, base_channel)
 
     def pulse(self,ts):
     	while ts < 0.2e-3:
