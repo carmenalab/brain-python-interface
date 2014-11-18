261b033d33fb97f3f1135e347a139a56587ffb35
Siddharth Dangi <siddharthdangi@gmail.com>
2014-11-17 17:16:15 -0800

    minor

--- db/namelist.py
+++ db/namelist.py
@@ -24,6 +24,7 @@ features = dict(
     motion_autoalign=features.MotionAutoAlign,
     bmi=features.PlexonBMI,
     blackrockbmi=features.BlackrockBMI,
+    brainampdata=features.BrainAmpData,
     saveHDF=features.SaveHDF,
     relay_plexon=features.RelayPlexon,
     relay_plexbyte=features.RelayPlexByte,
