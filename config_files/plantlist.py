b28a0ce95e0b6a7151edb9aa18eee15041169a94
Arc Reactor <arc@arc>
2015-05-21 17:52:42 -0700

    WARNING: may break things! BMI-specific code db/namelist.py moved to bmilist.py (not version controlled), in directory config_files. plantlist.py also moved to that directory

---
+++ config_files/plantlist.py
@@ -0,0 +1,50 @@
+'''
+List of possible "plants" that a subject could control either during manual or brain control
+'''
+import numpy as np
+from riglib import plants
+
+pi = np.pi
+RED = (1,0,0,.5)
+## BMI Plants
+cursor_14x14 = plants.CursorPlant(endpt_bounds=(-14, 14, 0., 0., -14, 14))
+cursor_25x14 = plants.CursorPlant(endpt_bounds=(-25, 25, 0., 0., -14, 14))
+cursor_14x14_no_vel_wall = plants.CursorPlant(endpt_bounds=(-14, 14, 0., 0., -14, 14), vel_wall=False)
+
+chain_kwargs = dict(link_radii=.6, joint_radii=0.6, joint_colors=(181/256., 116/256., 96/256., 1), link_colors=(181/256., 116/256., 96/256., 1))
+shoulder_anchor = np.array([2., 0., -15])
+chain_15_15_5_5 = plants.RobotArmGen2D(link_lengths=[15, 15, 5, 5], base_loc=shoulder_anchor, **chain_kwargs)
+init_joint_pos = np.array([ 0.47515737,  1.1369006 ,  1.57079633,  0.29316668])
+chain_15_15_5_5.set_intrinsic_coordinates(init_joint_pos)
+
+chain_15_15_5_5_on_screen = plants.RobotArmGen2D(link_lengths=[15, 15, 5, 5], base_loc=shoulder_anchor, stay_on_screen=True, **chain_kwargs)
+chain_15_15_5_5_on_screen.set_intrinsic_coordinates(init_joint_pos)
+
+
+chain_20_20_endpt = plants.EndptControlled2LArm(link_lengths=[20, 20], base_loc=shoulder_anchor, **chain_kwargs)
+init_pos = np.array([0, 0, 0], np.float64)
+chain_20_20_endpt.set_intrinsic_coordinates(init_pos)
+
+chain_20_20 = plants.RobotArmGen2D(link_lengths=[20, 20], base_loc=shoulder_anchor, **chain_kwargs)
+init_pos = np.array([pi/2, pi/2])
+init_pos = np.array([ 0.38118002,  2.08145271])
+chain_20_20.set_intrinsic_coordinates(init_pos)
+
+passive_exo = plants.UpperArmPassiveExo()
+
+cursor_onedimLFP = plants.onedimLFP_CursorPlant(endpt_bounds=(-14, 14, 0., 0., -14, 14), lfp_cursor_rad=1.5, lfp_cursor_color=(248/256., 220/256., 0/256., 1))
+cursor_onedimLFP_or = plants.onedimLFP_CursorPlant(endpt_bounds=(-14, 14, 0., 0., -14, 14), lfp_cursor_rad=1.5, lfp_cursor_color=(250/256., 102/256., 0/256., 1))
+inv_cursor_onedimLFP = plants.onedimLFP_CursorPlant_inverted(endpt_bounds=(-14, 14, 0., 0., -14, 14), lfp_cursor_rad=1.5, lfp_cursor_color=(248/256., 220/256., 0/256., 1))
+
+plantlist = dict(
+	cursor_14x14=cursor_14x14, 
+	cursor_25x14=cursor_25x14, 
+	chain_15_15_5_5=chain_15_15_5_5, 
+	chain_15_15_5_5_on_screen=chain_15_15_5_5_on_screen, 
+	passive_exo=passive_exo, 
+	chain_20_20=chain_20_20, 
+	chain_20_20_endpt=chain_20_20_endpt, 
+	cursor_onedimLFP=cursor_onedimLFP, 
+    cursor_onedimLFP_or=cursor_onedimLFP_or,
+    inv_cursor_onedimLFP=inv_cursor_onedimLFP)
+
--- db/namelist.py
+++ db/namelist.py
@@ -39,7 +39,6 @@ generators = dict()
 for fn_name, fn in izip(generator_names, generator_functions):
     generators[fn_name] = fn
 
-
 from tracker import models
 class SubclassDict(dict):
     '''
@@ -62,71 +61,14 @@ instance_to_model = SubclassDict( {
     Decoder: models.Decoder,
 } )
 
-
-bmi_algorithms = dict(
-    KFDecoder=bmi.train.train_KFDecoder,
-    PPFDecoder=bmi.train.train_PPFDecoder,
-    OneDimLFPDecoder=bmi.train.create_onedimLFP,
-)
-
-bmi_training_pos_vars = [
-    'cursor',
-    'joint_angles',
-    'plant_pos',  # used for ibmi tasks
-    'mouse_state',
-    'decoder_state',
-]
-
-#################################
-##### State-space models for BMIs
-#################################
-joint_2D_state_space = bmi.state_space_models.StateSpaceNLinkPlanarChain(n_links=2, w=0.01)
-tentacle_2D_state_space = bmi.state_space_models.StateSpaceNLinkPlanarChain(n_links=4, w=0.01)
-
-from tasks.ismore_bmi_lib import StateSpaceArmAssist, StateSpaceReHand, StateSpaceIsMore
-from tasks.point_mass_cursor import PointForceStateSpace
-armassist_state_space = StateSpaceArmAssist()
-rehand_state_space = StateSpaceReHand()
-ismore_state_space = StateSpaceIsMore()
-
-## Velocity SSMs
-from riglib.bmi.state_space_models import offset_state, State
-endpt_2D_states = [State('hand_px', stochastic=False, drives_obs=False, min_val=-25., max_val=25., order=0),
-                   State('hand_py', stochastic=False, drives_obs=False, order=0),
-                   State('hand_pz', stochastic=False, drives_obs=False, min_val=-14., max_val=14., order=0),
-                   State('hand_vx', stochastic=True,  drives_obs=True, order=1),
-                   State('hand_vy', stochastic=False, drives_obs=False, order=1),
-                   State('hand_vz', stochastic=True,  drives_obs=True, order=1),
-                   offset_state]
-endpt_2D_state_space = state_space_models.LinearVelocityStateSpace(endpt_2D_states)
-
-from tasks.speller_tasks import PointClickSSM
-mouse_ssm = PointClickSSM()
-
-bmi_state_space_models=dict(
-    Endpt2D=endpt_2D_state_space,
-    Tentacle=tentacle_2D_state_space,
-    Armassist=armassist_state_space,
-    Rehand=rehand_state_space,
-    ISMORE=ismore_state_space,
-    Joint2L=joint_2D_state_space,
-    Mouse=mouse_ssm,
-    PointMass=PointForceStateSpace(),
-)
-
-extractors = dict(
-    spikecounts = bmi.extractor.BinnedSpikeCountsExtractor,
-    LFPpowerMTM = bmi.extractor.LFPMTMPowerExtractor,
-    LFPpowerBPF = bmi.extractor.LFPButterBPFPowerExtractor,
-    EMGAmplitude = bmi.extractor.EMGAmplitudeExtractor,
-)
-
-from riglib.bmi import train
-kin_extractors = dict(
-    pos_vel=train.get_plant_pos_vel,
-    null=train.null_kin_extractor,
-)
-
-default_extractor = "spikecounts"
-
-bmi_update_rates = [10, 20, 30, 60, 120, 180]
+try:
+    from bmilist import bmi_algorithms
+    from bmilist import bmi_training_pos_vars
+    from bmilist import bmi_state_space_models
+    from bmilist import extractors
+    from bmilist import default_extractor
+    from bmilist import bmi_update_rates
+except:
+    import "error importing BMI configuration variables"
+    import traceback
+    traceback.print_exc()
