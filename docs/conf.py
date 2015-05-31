de349915e87bc01da1e8fc21460a463d0e08a467
Suraj Gowda <surajgowda33@gmail.com>
2015-05-31 14:29:26 -0700

    updating authorship, copyright, and open-source license information

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BMI3D'
-copyright = u'2012, James Gao'
+copyright = u'2012, James Gao; 2015, James Gao, Helene Moorman, Suraj Gowda, Siddharth Dangi'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
