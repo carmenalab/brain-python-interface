0284a40004dbb404f740bad9c71388c5ecc04c5e
Suraj Gowda <surajgowda33@gmail.com>
2015-06-04 11:16:40 -0700

    manual merge of test cases from remotes/bmi3d/ibmi

---
+++ tests/ibmi/brainamp/basic_brainamp_test.py
@@ -0,0 +1,42 @@
+import numpy as np
+import time
+import scipy.io as sio
+
+from riglib.ismore import brainamp_channel_lists
+from riglib.brainamp.rda import *
+
+
+
+fs = 1000
+channels = brainamp_channel_lists.emg_eog2_eeg
+
+total_time = 120  # how many secs of data to receive and save
+
+n_samples = 2 * fs * total_time  # allocate twice as much space as expected
+n_chan = len(channels)
+
+
+DATA = np.zeros((n_chan, n_samples))
+idxs = np.zeros(n_chan, int)
+
+chan_to_row = dict()
+for row, chan in enumerate(channels):
+    chan_to_row[chan] = row
+
+emgdata_obj = EMGData()
+emgdata_obj.start()
+
+start_time = time.time()
+
+while (time.time() - start_time) < total_time:
+    chan, data = emgdata_obj.get()
+
+    row = chan_to_row[chan]
+    idx = idxs[row]
+
+    DATA[row, idx] = data['data']
+    idxs[row] += 1
+
+
+save_dict = {'data': DATA}
+sio.matlab.savemat('brainamp_data.mat', save_dict)
---
+++ tests/ibmi/test_KF_EMG_decoding.py
@@ -0,0 +1,79 @@
+'''
+To use on command line, cd into the directory containing this file and type:
+    python test_KF_EMG_decoding.py <full path to .mat file>
+e.g., 
+    python test_KF_EMG_decoding.py /Users/sdangi/Dropbox/KF_data/dataset1.mat
+'''
+
+
+import scipy.io as sio
+import numpy as np
+from scipy.stats import pearsonr
+import ntpath
+import sys
+import fnmatch
+
+from riglib.bmi import bmi, kfdecoder
+
+# name of .mat file is passed as first argument
+matfile = sys.argv[1]
+
+# load variables from .mat file
+mat = sio.loadmat(matfile, struct_as_record=False, squeeze_me=True)
+for var_name in mat.keys():
+    if fnmatch.fnmatch(var_name, 'emg*_train*'):
+        emg_train = mat[var_name]
+    elif fnmatch.fnmatch(var_name, 'kin*_train*'):
+        kin_train = mat[var_name]
+    elif fnmatch.fnmatch(var_name, 'emg*_test*'):
+        emg_test = mat[var_name]
+    elif fnmatch.fnmatch(var_name, 'kin*_test*'):
+        kin_test = mat[var_name]
+
+# fit KF matrices from training data
+A, W = kfdecoder.KalmanFilter.MLE_state_space_model(kin_train, include_offset=True)
+C, Q = kfdecoder.KalmanFilter.MLE_obs_model(kin_train, emg_train)
+
+# create a KalmanFilter object
+kf = kfdecoder.KalmanFilter(A, W, C, Q)
+
+# calculate converged K and P matrices (without doing any KF predictions)
+# this function will print out how many iterations it takes for the matrix norm
+#   of the difference between K matrices on successive iterations to go below
+#   the tolerance value 'tol'
+F, K, P = kf.get_sskf(return_P=True, verbose=True, tol=1e-15)
+
+# define the starting state of the KF to be zeros (with a 1 for the last entry)
+len_x = kin_test.shape[0] + 1
+x_mean = np.zeros((len_x, 1))
+x_mean[-1] = 1
+x_cov = np.zeros((len_x, len_x))
+x = bmi.GaussianState(x_mean, x_cov)
+
+# predictions will be stored in this matrixs
+kin_pred = np.zeros((len_x, kin_test.shape[1]))
+
+# iterate through each column of emg_test and predict the kinematics
+for i, y in enumerate(emg_test.T):
+    y = y.reshape(-1, 1)
+    x = kf._forward_infer(x, y)
+    kin_pred[:, i] = np.array(x.mean).reshape(-1)
+
+# calculate the correlation coefficient for each state variable
+cc_values = np.array([pearsonr(kin_test[i, :], kin_pred[i, :])[0] for i in range(len_x-1)])
+
+print 'datafile:', matfile
+print 'cc values:', cc_values
+
+# save the results into a .mat file
+vars_to_save = {
+    'A': A,
+    'W': W,
+    'C': C,
+    'Q': Q,
+    'K': K,
+    'P': P,
+    'cc_values': cc_values,
+}
+sio.matlab.savemat(matfile[:-4] + '_pyresults.mat', vars_to_save)
+
