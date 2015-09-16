05bfbafc457121619d85e008973d591b66fc24e8
Arc Reactor <arc@arc>
2015-09-15 18:53:54 -0700

    removed blocking _start_reward and _start_wait functions. This needs better handling with __getattr__ in the experiment class

--- riglib/stereo_opengl/window.py
+++ riglib/stereo_opengl/window.py
@@ -140,12 +140,6 @@ class Window(LogExperiment):
     def _start_None(self):
         pygame.display.quit()
 
-    def _start_reward(self):
-        pass
-
-    def _start_wait(self):
-        pass
-    
     def _test_stop(self, ts):
         '''
         Stop the task if the escape key is pressed, or if the super _test_stop instructs a stop
