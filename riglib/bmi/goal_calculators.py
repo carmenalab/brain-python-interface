2263348663b8d5743370f5c9d0062e473848f268
Suraj Gowda <surajgowda33@gmail.com>
2015-09-22 01:50:40 -0700

    more docs

--- riglib/bmi/goal_calculators.py
+++ riglib/bmi/goal_calculators.py
@@ -23,45 +23,51 @@ class ZeroVelocityGoal(GoalCalculator):
     '''
     Assumes that the target state of the BMI is to move to the task-specified position with zero velocity
     '''
-    def __init__(self, ssm):
+    def __init__(self, ssm=None):
         '''
         Constructor for ZeroVelocityGoal
     
         Parameters
         ----------
         ssm : state_space_models.StateSpace instance
-            The state-space model of the Decoder that is being assisted/adapted. Currently unused
+            The state-space model of the Decoder that is being assisted/adapted. Not needed for this particular method
     
         Returns
         -------
         ZeroVelocityGoal instance
         '''
-        # assert ssm == train.endpt_2D_state_space
         self.ssm = ssm
 
     def __call__(self, target_pos, **kwargs):
         '''
-        Docstring
+        Calculate the goal state [p, 0, 1] where p is the n-dim position and 0 is the n-dim velocity
     
         Parameters
         ----------
-        target_pos : DATA_TYPE
-            ARG_DESCR
+        target_pos : np.ndarray
+            Optimal position, in generalized coordinates (i.e., need not be cartesian coordinates)
         kwargs : optional kwargs
-            ARG_DESCR
+            These are ignored, just present for function call compatibility
     
         Returns
         -------
-        
+        np.ndarray
+            (N, 1) indicating the target state
         '''
         target_vel = np.zeros_like(target_pos)
         offset_val = 1
         error = 0
-        target_state = np.hstack([target_pos, target_vel, 1])
-        return (target_state, error), True
+        target_state = np.hstack([target_pos, target_vel, 1]).reshape(-1, 1)
+        return target_state
 
 class ZeroVelocityAccelGoal(ZeroVelocityGoal):
+    '''
+    Similar to ZeroVelocityGoal, but used for a second order system where you also want the goal acceleration to be zero.
+    '''
     def __call__(self, target_pos, **kwargs):
+        '''
+        See ZeroVelocityGoal.__call__ for argument documentation
+        '''
         target_vel = np.zeros_like(target_pos)
         target_acc = np.zeros_like(target_pos)
         offset_val = 1
@@ -71,11 +77,13 @@ class ZeroVelocityAccelGoal(ZeroVelocityGoal):
 
 
 class PlanarMultiLinkJointGoal(GoalCalculator, mp_calc.FuncProxy):
-    '''    Docstring    '''
+    '''
+    Looks up goal configuration for a redundant system based on the endpoint goal and tries to find the closest solution.
+
+    DEPRECATED: The method implemented has not been used for a long time, and is not the best method for achieving finding the "closest" config space solution as desired.
+    '''
     def __init__(self, ssm, shoulder_anchor, kin_chain, multiproc=False, init_resp=None):
-        '''    Docstring    '''
         def fn(target_pos, **kwargs):
-            '''    Docstring    '''
             joint_pos = kin_chain.inverse_kinematics(target_pos, **kwargs)
             endpt_error = np.linalg.norm(kin_chain.endpoint_pos(joint_pos) - target_pos)
 
@@ -85,9 +93,35 @@ class PlanarMultiLinkJointGoal(GoalCalculator, mp_calc.FuncProxy):
         super(PlanarMultiLinkJointGoal, self).__init__(fn, multiproc=multiproc, waiting_resp='prev', init_resp=init_resp)
 
 class PlanarMultiLinkJointGoalCached(GoalCalculator, mp_calc.FuncProxy):
-    '''    Docstring    '''
+    '''
+    Determine the goal state of a redundant system by look-up-table, i.e. redundancy is collapsed 
+    by arbitrary mapping between redudnant target space and configuration space
+
+    TODO: since multiprocessing is not required for this class, it needs to do a better job of hiding the multiprocessing.
+    '''
     def __init__(self, ssm, shoulder_anchor, kin_chain, multiproc=False, init_resp=None, **kwargs):
-        '''    Docstring    '''
+        '''
+        Constructor for PlanarMultiLinkJointGoalCached
+
+        Parameters
+        ----------
+        ssm : state_space_models.StateSpace instance
+        shoulder_anchor : np.ndarray of shape (3,)
+            Position of the manipulator anchor
+        kin_chain : robot_arms.KinematicChain instance
+            Object representing the kinematic chain linkages (D-H parameters)
+        multiproc : bool, optional, default=False
+            Should leave this false for this 'cached' method
+        init_resp : None
+            Ignore this if multiproc=False, as directed above.
+        kwargs : optional keyword arguments
+            Can pass in 'goal_cache_block' to specify from which task entry to grab the cache file. WARNING: this is currently commented out
+
+        Returns
+        -------
+        PlanarMultiLinkJointGoalCached instance
+
+        '''
         self.ssm = ssm
         self.shoulder_anchor = shoulder_anchor
         self.kin_chain = kin_chain
@@ -121,24 +155,33 @@ class PlanarMultiLinkJointGoalCached(GoalCalculator, mp_calc.FuncProxy):
         super(PlanarMultiLinkJointGoalCached, self).__init__(fn, multiproc=multiproc, waiting_resp='prev', init_resp=init_resp)
 
     def __call__(self, target_pos, **kwargs):
-        '''    Docstring    '''
+        '''
+        Calculate the goal state [p, 0, 1] where p is the n-dim position and 0 is the n-dim velocity
+        p is the configuration space position, which must be looked up based on the target_pos 
+        (not a one-to-one mapping in general)
+    
+        Parameters
+        ----------
+        target_pos : np.ndarray
+            Optimal position, in generalized coordinates (i.e., need not be cartesian coordinates)
+        kwargs : optional kwargs
+            These are ignored, just present for function call compatibility
+    
+        Returns
+        -------
+        np.ndarray
+            (N, 1) indicating the target state
+        '''
         joint_pos = None
         for pos in self.cached_data:
             if np.linalg.norm(target_pos - np.array(pos)) < 0.001:
                 joint_pos = self.cached_data[pos]
-                # possible_joint_pos = self.cached_data[pos]
-                # ind = np.random.randint(0, len(possible_joint_pos))
-                # joint_pos = possible_joint_pos[ind]
-                # break
-
-        # print joint_pos
 
         if joint_pos == None:
             raise ValueError("Unknown target position!: %s" % str(target_pos))
 
         target_state = np.hstack([joint_pos, np.zeros_like(joint_pos), 1])
         
-        # TODO These are wrong!
-        endpt_error = 0 #np.linalg.norm(kin_chain.endpoint_pos(joint_pos) - endpt_location)
+        endpt_error = 0
 
         return (target_state, endpt_error), True
