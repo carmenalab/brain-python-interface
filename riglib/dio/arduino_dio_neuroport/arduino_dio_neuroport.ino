0107b2f5384387faa2aa731310400c65a1822b46
Suraj Gowda <surajgowda33@gmail.com>
2015-10-05 20:53:44 -0700

    changed d15 pin for blackrock back to consecutive order

--- riglib/dio/arduino_dio_neuroport/arduino_dio_neuroport.ino
+++ riglib/dio/arduino_dio_neuroport/arduino_dio_neuroport.ino
@@ -6,7 +6,7 @@ char c;
 char d;
 int en = 0;
 char dio_data[2];
-int data_pins[] = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 48};
+int data_pins[] = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
 
 int rstart_init = 0;
 
