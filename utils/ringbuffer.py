9616bbd94e9265773e4feccb8dd8e4b43548bcba
Tubingen <ismorelinux2@tubingen>
2015-10-28 03:14:05 +0100

    added function add multiple items to the buffer all at once

--- utils/ringbuffer.py
+++ utils/ringbuffer.py
@@ -15,6 +15,12 @@ class RingBuffer(object):
         self.idx = (self.idx + 1) % self.capacity
         self.n_items = min(self.n_items + 1, self.capacity)
 
+    def add_multiple_values(self, item):
+        l = item.shape[1]
+        self.buffer[:, self.idx:self.idx + l] = item
+        self.idx = (self.idx + l) % self.capacity
+        self.n_items = min(self.n_items + l, self.capacity)
+        
     def get(self, n_items):
         if n_items > self.n_items:
             return None
