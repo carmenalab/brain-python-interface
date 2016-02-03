9dddc295c2cddd3ef47e74686aced5857f911f9b
preeya <pkhanna104@gmail.com>
2016-02-03 12:10:53 -0800

    wrap around condition added to ring buffer for ismore

--- utils/ringbuffer.py
+++ utils/ringbuffer.py
@@ -16,9 +16,16 @@ class RingBuffer(object):
         self.n_items = min(self.n_items + 1, self.capacity)
 
     def add_multiple_values(self, item):
-        l = item.shape[1]
-        self.buffer[:, self.idx:self.idx + l] = item
-        self.idx = (self.idx + l) % self.capacity
+        l = item.shape[1]    
+        
+        if (self.idx + l) <= self.capacity:
+            self.buffer[:, self.idx:self.idx + l] = item
+            self.idx = (self.idx + l) % self.capacity
+        else:
+            self.new_idx =  (self.idx + l) % self.capacity
+            self.buffer[:, self.idx:] = item[:,0:l-self.new_idx]
+            self.buffer[:, :self.new_idx] = item[:,l-self.new_idx:]
+            self.idx = self.new_idx
         self.n_items = min(self.n_items + l, self.capacity)
         
     def get(self, n_items):
