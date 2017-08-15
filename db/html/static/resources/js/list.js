cfe1281b1af27a4e59f294b23aff8c19966a9438
ismorelinux <ismorelinux@tecnalia>
2017-08-15 15:20:50 +0200

    added line so now generator params update when makign new generator

--- db/html/static/resources/js/list.js
+++ db/html/static/resources/js/list.js
@@ -82,8 +82,9 @@ Sequence.prototype.update = function(info) {
         var seq_obj = this;
         this._handle_chlist = function () {
             var id = this.value; // 'this' is bound to the options list when it's used inside the callback below
-            if (id == "new")
+            if (id == "new") {
                 seq_obj.edit()
+            }
             else {
             	// the selected sequence is a previously used sequence, so populate the parameters from the db
                 seq_obj.params.update(info[id].params);
@@ -99,6 +100,7 @@ Sequence.prototype.update = function(info) {
             }
         };
         $("#seqlist").change(this._handle_chlist);
+        $("#seqgen").change(this._handle_chgen);
 
         $("#seqstatic,#seqparams input, #seqgen").attr("disabled", "disabled");
     } else {
