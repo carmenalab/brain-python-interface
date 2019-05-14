10b19a91645853546647e5a64e6fa2641d8a5c1b
Tomek Fraczek <tfraczek@uw.edu>
2019-05-14 14:09:00 -0700

    Comment out all plexon setup, seems to try and use python2

--- install/docker/src_code_install.sh
+++ install/docker/src_code_install.sh
@@ -10,15 +10,15 @@ git clone https://github.com/sgowda/robotics_toolbox $HOME/code/robotics
 # pygame
 hg clone https://bitbucket.org/pygame/pygame $HOME/code/pygame
 # Phidgets code
-wget https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22/libphidget22-1.1.20190417.tar.gz
-wget https://www.phidgets.com/downloads/phidget22/libraries/any/Phidget22Python/Phidget22Python_1.1.20190418.zip
+#wget https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22/libphidget22-1.1.20190417.tar.gz
+#wget https://www.phidgets.com/downloads/phidget22/libraries/any/Phidget22Python/Phidget22Python_1.1.20190418.zip
 
 
 ####### Install source code, configure software
 # plexread module
-cd $BMI3D/riglib
-echo which python
-python setup.py install
+#cd $BMI3D/riglib
+which python
+#python setup.py install
 
 # pygame
 cd $HOME/code/pygame
@@ -27,25 +27,27 @@ python setup.py install
 # NIDAQ software -- deprecated!
 # $HOME/code/bmi3d/riglib/nidaq/build.sh
 
-# Phidgets libraries
-cd $CODE/src/
-tar xzf libphidget.tar.gz 
-cd libphidget*
-./configure
-make
-make install
+echo "TESTED IF HERE"
 
-cd $CODE/src/
-unzip PhidgetsPython.zip  
-cd PhidgetsPython
-python setup.py install
+# Phidgets libraries
+#cd $CODE/src/
+#tar xzf libphidget.tar.gz 
+#cd libphidget*
+#./configure
+#make
+#make install
+
+#cd $CODE/src/
+#unzip PhidgetsPython.zip  
+#cd PhidgetsPython
+#python setup.py install
 
 
 
 ####### Configure udev rules, permissions
 # Phidgets
-cp $CODE/src/libphidget*/udev/99-phidgets.rules /etc/udev/rules.d
-chmod a+r /etc/udev/rules.d/99-phidgets.rules
+#cp $CODE/src/libphidget*/udev/99-phidgets.rules /etc/udev/rules.d
+#chmod a+r /etc/udev/rules.d/99-phidgets.rules
 # NIDAQ
 cp $HOME/code/bmi3d/install/udev/comedi.rules /etc/udev/rules.d/
 chmod a+r /etc/udev/rules.d/comedi.rules 
@@ -61,7 +63,7 @@ sed -i '$a export BMI3D=/home/lab/code/bmi3d' $HOME/.bashrc
 sed -i '$a source $HOME/code/bmi3d/pathconfig.sh' $HOME/.bashrc
 source $HOME/.bashrc
 
-chown -R $USER ~/.matplotlib
+chown -R $USER ~/.matplotlibs
 
 cd $HOME/code/bmi3d/db
 python manage.py syncdb
