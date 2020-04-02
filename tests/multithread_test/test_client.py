b037105b437ce877d9e20bcd1bf3f8d6e85c0b00
Sijia Li <sijia66@uw.edu>
2020-04-02 15:12:36 -0700

    commit -m 'save to mocap simulation to tests'

---
+++ tests/multithread_test/test_client.py
@@ -0,0 +1,51 @@
+import socket
+import struct
+from threading import Thread
+
+class TestClient:
+    def __init__(self):
+        # Change this value to the IP address of the NatNet server.
+        self.serverIPAddress = "127.0.0.1" 
+
+        # Change this value to the IP address of your local network interface
+        self.localIPAddress = "127.0.0.1"
+
+        self.dataPort = 5005
+
+        # similar to the rigidBodyListener 
+        self.dataListener = None
+
+        # Create a data socket to attach to the NatNet stream
+    def __createDataSocket( self, port ):
+        result = socket.socket( socket.AF_INET,     # Internet
+                              socket.SOCK_DGRAM,
+                              socket.IPPROTO_UDP)    # UDP
+
+        #result.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        
+        #result.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(self.multicastAddress) + socket.inet_aton(self.localIPAddress))
+
+        result.bind( (self.localIPAddress, port) )
+
+        return result
+
+    def __dataThreadFunction( self, socket ):
+        while True:
+            # Block for input
+            data, addr = socket.recvfrom( 32768 ) # 32k byte buffer size
+            if( len( data ) > 0 ):
+                #self.__processMessage( data )
+
+                        # Send information to any listener.
+                if self.dataListener is not None:
+                    self.dataListener(data)
+
+    def run( self ):
+        # Create the data socket
+        self.dataSocket = self.__createDataSocket( self.dataPort )
+        if( self.dataSocket is None ):
+            print( "Could not open data channel" )
+            exit
+
+        # Create a separate thread for receiving data packets
+        dataThread = Thread( target = self.__dataThreadFunction, args = (self.dataSocket, ))
+        dataThread.start()
