549e0cfabcb06768a800b8c18330c5e16da98e33
Sijia Li <sijia66@uw.edu>
2020-04-10 16:09:16 -0700

    seem to fix django start sequence

--- tests/test_dbfunctions.py
+++ tests/test_dbfunctions.py
@@ -2,26 +2,34 @@
 '''
 A set of tests to ensure that all the dbfunctions are still functional
 '''
-from db import dbfunctions as dbfn
-reload(dbfn)
+import os 
+import django
 
-id = 4150
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db.settings')
+django.setup()
+
+from tracker import models
+import db.dbfunctions as dbfn
+
+id = 77
 te = dbfn.TaskEntry(id)
 
-print(dbfn.get_plx_file(id))
-print(dbfn.get_decoder_name(id))
-print(dbfn.get_decoder_name_full(id))
-print(dbfn.get_decoder(id))
-print(dbfn.get_params(id))
-print(dbfn.get_param(id, 'decoder'))
 print(dbfn.get_date(id))
 print(dbfn.get_notes(id))
 print(dbfn.get_subject(id))
 print(dbfn.get_length(id))
 print(dbfn.get_success_rate(id))
 
-id = 1956
-print(dbfn.get_bmiparams_file(id))
+#print(dbfn.get_plx_file(id))
+#print(dbfn.get_decoder_name(id))
+#print(dbfn.get_decoder_name_full(id))
+#print(dbfn.get_decoder(id))
+#print(dbfn.get_params(id))
+#print(dbfn.get_param(id, 'decoder'))
+
+
+#id = 1956
+#print(dbfn.get_bmiparams_file(id))
 
 # TODO check blackrock file fns
 
