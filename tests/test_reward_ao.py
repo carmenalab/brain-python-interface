b3509ca516e4d9b62c1e7454940548584709f542
Sijia Li <sijia66@uw.edu>
2020-06-02 16:01:14 -0700

    set up tests for reward system

---
+++ tests/test_reward_ao.py
@@ -0,0 +1,27 @@
+from riglib import ao_reward
+
+import unittest
+
+class TestAoReward(unittest.TestCase):
+
+    def setUp(self):
+        self.reward_sys = ao_reward.Basic()
+
+
+    @unittest.skip("not sure which method to use")
+    def test_connection(self):
+        pass
+
+    def test_flow_out(self):
+        reward_time = 1000 #ms
+        self.reward_sys(reward_time)
+
+    @unittest.skip("not sure how to calibrate yet")
+    def test_calibration(self):
+        print('fill up bottle')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
