87cff70f352db47ceced74cf753737b5e3a405a4
Suraj Gowda <surajgowda33@gmail.com>
2020-08-13 08:01:35 -0700

    new report log saving

--- built_in_tasks/passivetasks.py
+++ built_in_tasks/passivetasks.py
@@ -1,10 +1,6 @@
 '''
 Tasks which control a plant under pure machine control. Used typically for initializing BMI decoder parameters.
 '''
-
-
-
-
 import numpy as np
 import time
 import os
@@ -22,14 +18,13 @@ from riglib.bmi import clda, assist, extractor, train, goal_calculators, ppfdeco
 from riglib.bmi.bmi import Decoder, BMISystem, GaussianStateHMM, BMILoop, GaussianState, MachineOnlyFilter
 from riglib.bmi.extractor import DummyExtractor
 from riglib.stereo_opengl.window import WindowDispl2D, FakeWindow
-
+from riglib.bmi.state_space_models import StateSpaceEndptVel2D
 
 from .bmimultitasks import BMIControlMulti
 
 
 bmi_ssm_options = ['Endpt2D', 'Tentacle', 'Joint2L']
 
-from riglib.bmi.state_space_models import StateSpaceEndptVel2D
 class EndPostureFeedbackController(BMILoop, traits.HasTraits):
     ssm_type_options = bmi_ssm_options
     ssm_type = traits.OptionsList(*bmi_ssm_options, bmi3d_input_options=bmi_ssm_options)
@@ -69,12 +64,16 @@ class TargetCaptureVFB2DWindow(TargetCaptureVisualFeedback, WindowDispl2D):
 
     @classmethod
     def get_desc(cls, params, report):
-        if report is not None and len(report) > 0:
+        if isinstance(report, list) and len(report) > 0:
             duration = report[-1][-1] - report[0][-1]
             reward_count = 0
             for item in report:
                 if item[0] == "reward":
                     reward_count += 1
+            return "{} rewarded trials in {} min".format(reward_count, int(np.ceil(duration / 60)))
+        elif isinstance(report, dict):
+            duration = report['runtime'] / 60
+            reward_count = report['n_success_trials']
             return "{} rewarded trials in {} min".format(reward_count, int(np.ceil(duration / 60)))
         else:
             return "No trials"
