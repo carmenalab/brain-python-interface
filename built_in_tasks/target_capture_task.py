b6018a1c4fe85e93599dc6f388892eabba68fe55
sijia <leo.scholl@gmail.com>
2021-04-23 09:49:57 -0700

    move middle target

--- built_in_tasks/target_capture_task.py
+++ built_in_tasks/target_capture_task.py
@@ -735,9 +735,9 @@ class ScreenReachAngle(ScreenTargetCapture):
         [ntrials x ntargets x 3] array of target coordinates
         '''
         targets = np.array([
-            [0.5, 1],
             [0, 0.5],
             [1, 0.5],
+            [0.5, 1],
             [1, 0],
             [0, 0],
             [0.25, 0.25],
