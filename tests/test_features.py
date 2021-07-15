185775e941770a54e0723ef69f650cacc72711f4
Leo Scholl <leo.scholl@gmail.com>
2021-07-15 09:34:24 -0700

    switch to multiprocessing

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,8 +1,10 @@
 from riglib import experiment
 from built_in_tasks.manualcontrolmultitasks import ManualControl
+from built_in_tasks.othertasks import LaserConditions
 from riglib.stereo_opengl.window import WindowDispl2D
 from features.peripheral_device_features import KeyboardControl, MouseControl
 import features.sync_features as sync_features
+from features.laser_features import CrystaLaser
 import numpy as np
 
 import unittest
@@ -45,8 +47,7 @@ class TestLaser(unittest.TestCase):
         self.assertCountEqual(laser1.edges, [0, 1])
         laser1.set_square_wave(1, 5)
         self.assertCountEqual(laser1.edges, np.linspace(0, 5.0, 11))
-        laser1.start()
-        laser1.join()
+        laser1.run() # run on the same thread so we can make changes to the test gpio state
         print(gpio.value[1,:])
         self.assertEqual(sum(gpio.value[1,:]), 6) # 1 Hz over 5 seconds has 6 positive edges including the last one at 5 s
 
@@ -70,8 +71,46 @@ class TestLaser(unittest.TestCase):
         laser.start()
         laser.join()
 
-    def test_laser_trials(self):
-        pass
+    @unittest.skip("Need arduino connected for this to pass")
+    def test_laser_conditions(self):
+        Exp = experiment.make(LaserConditions, [CrystaLaser])
+        exp = Exp(LaserConditions.single_laser_pulse(nreps=2))
+        exp.init()
+        exp.run()
+
+    def test_dio_pulse_width(self):
+        from riglib.gpio import ArduinoGPIO, DigitalWave
+        import time
+        gpio = ArduinoGPIO('/dev/crystalaser')
+
+        pulse_widths = [0.00001, 0.00005, 0.0001, 0.0002, 0.0005, 0.001]
+        reps = 20
+        durations = np.zeros((len(pulse_widths),reps))
+        for width_idx, width in enumerate(pulse_widths):
+            for n in range(reps):
+                t0 = time.perf_counter()
+                gpio.write_many(1<<12, 1<<12)
+                while (time.perf_counter() - t0 < width):
+                    pass
+                durations[width_idx, n] = time.perf_counter() - t0
+                gpio.write_many(1<<12, 0)
+                time.sleep(0.1)
+
+        print(np.mean(durations, axis=1))
+        print(np.std(durations, axis=1))
+
+        time.sleep(1)
+
+        seq = LaserConditions.single_laser_pulse(nreps=reps, duration=pulse_widths, uniformsampling=False, ascending=True)
+        print(seq)
+        for idx, powers, edges in seq:
+            edges = edges[0]
+            wave = DigitalWave(gpio, mask=1<<12)
+            wave.set_edges(edges, True)
+            wave.start()
+            print(edges)
+            wave.join()
+            time.sleep(0.1)
 
 class TestSync(unittest.TestCase):
 
