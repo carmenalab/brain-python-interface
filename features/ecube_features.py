51363f42d55992676badccd40c55fe599a893e03
sijia <leo.scholl@gmail.com>
2022-02-07 08:52:12 -0800

    tabs formatting

--- features/ecube_features.py
+++ features/ecube_features.py
@@ -35,9 +35,9 @@ class RecordECube(traits.HasTraits):
     headstage_channels = traits.Tuple((1, 1), desc="Range of headstage channels to record (1-indexed)")
     channel_mapping_file = traits.String("", desc="Name of channel mapping excel file")
     drmap_chamber = traits.String("", desc="Name of the recording chamber (e.g. LM1)")
-	drmap_drive_type = traits.String("", desc="Type of recording drive (e.g. ECoG244)")
-	drmap_implant_date = traits.String("", desc="Date recording drive was implanted (YYMMDD)")
-	drmap_config_date = traits.String("", desc="Date channel mapping was configured (YYMMDD)")
+    drmap_drive_type = traits.String("", desc="Type of recording drive (e.g. ECoG244)")
+    drmap_implant_date = traits.String("", desc="Date recording drive was implanted (YYMMDD)")
+    drmap_config_date = traits.String("", desc="Date channel mapping was configured (YYMMDD)")
     ecube_status = "Not initialized"
 
     def cleanup(self, database, saveid, **kwargs):
