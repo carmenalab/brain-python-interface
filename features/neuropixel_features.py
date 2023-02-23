8dbd863517a9903b13665bda888a012f0f69f8cd
pagaiisland <kperks@uw.edu>
2023-02-23 15:29:42 -0800

    installed open-ephys-python-tool

--- features/neuropixel_features.py
+++ features/neuropixel_features.py
@@ -4,6 +4,6 @@ Features for interacting with neuropixels
 import time
 import os
 import numpy as np
-#from open_ephys.control import OpenEphysHTTPServer
+from open_ephys.control import OpenEphysHTTPServer
 
 print(12)
