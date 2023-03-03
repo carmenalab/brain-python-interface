9574242e879313011c1fe997d2ea21f87d7a47f0
pagaiisland <kperks@uw.edu>
2023-03-03 12:59:50 -0800

    adaa

--- features/neuropixel_features.py
+++ features/neuropixel_features.py
@@ -6,7 +6,21 @@ import os
 import numpy as np
 from open_ephys.control import OpenEphysHTTPServer
 
-IP_neuropixel = '10.155.205.108'
-gui = OpenEphysHTTPServer(IP_neuropixel)
 
-gui.acquire(10)
+
+#gui.acquire(10)
+
+#gui.record(10)
+
+#print(gui.status())
+
+class RecordNeuropixels(traits.HasTraits):
+
+    self.IP_neuropixel = '10.155.205.108'
+    self.gui = OpenEphysHTTPServer(self.IP_neuropixel)
+
+    def cleanup(self, database, saveid, **kwargs):
+
+    @classmethod
+    def pre_init(cls, saveid=None, record_headstage=False, headstage_connector=None, headstage_channels=None, **kwargs):
+        gui.record()
