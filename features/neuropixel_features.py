451dec96513f0705a84ff139fb2e2740a09bd23c
pagaiisland <kperks@uw.edu>
2023-03-06 19:53:12 -0800

    Control openephys from bmi3d

--- features/neuropixel_features.py
+++ features/neuropixel_features.py
@@ -4,23 +4,44 @@ Features for interacting with neuropixels
 import time
 import os
 import numpy as np
+from riglib.experiment import traits
 from open_ephys.control import OpenEphysHTTPServer
-
-
-
-#gui.acquire(10)
-
-#gui.record(10)
-
-#print(gui.status())
 
 class RecordNeuropixels(traits.HasTraits):
 
-    self.IP_neuropixel = '10.155.205.108'
-    self.gui = OpenEphysHTTPServer(self.IP_neuropixel)
+    IP_neuropixel = '10.155.205.108'
+    gui = OpenEphysHTTPServer(IP_neuropixel)
 
-    def cleanup(self, database, saveid, **kwargs):
-
+    def cleanup(self,database, saveid, gui=gui, **kwargs):
+        super().cleanup(database, saveid, gui=gui, **kwargs)
+        try:
+            gui.acquire()
+        except Exception as e:
+            print(e)
+            print('\n\ncould not stop OpenEphys recording. Please manually stop the recording\n\n')
+        
     @classmethod
-    def pre_init(cls, saveid=None, record_headstage=False, headstage_connector=None, headstage_channels=None, **kwargs):
-        gui.record()
+    def pre_init(cls, saveid=None, gui=gui,**kwargs):
+        if saveid is not None:
+            try:
+                gui.record()
+            except Exception as e:
+                print(e)
+                print('\n\ncould not start OpenEphys recording\n\n')
+        else:
+            try:
+                gui.acquire()
+            except Exception as e:
+                print(e)
+                print('\n\ncould not start OpenEphys acquisition\n\n')
+        print(f'Open Ephys status : {gui.status()}')
+  
+        if hasattr(super(), 'pre_init'):
+            super().pre_init(saveid=saveid,gui=gui,**kwargs)
+
+    # def run(self):
+    #     super().run()
+    #     time.sleep(0.1) # Wait a bit to be sure the recording started
+    #     IP_neuropixel = '10.155.205.108'
+    #     gui = OpenEphysHTTPServer(IP_neuropixel)
+    #     print(f'Open Ephys status : {gui.status()}')
