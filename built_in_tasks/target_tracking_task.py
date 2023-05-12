9a25f7e7eb36b4a9db777ef943321fb01f5bc0e6
Leo Scholl <leo.scholl@gmail.com>
2023-05-11 18:16:37 -0700

    maybe fix tracking task

--- built_in_tasks/target_tracking_task.py
+++ built_in_tasks/target_tracking_task.py
@@ -93,6 +93,9 @@ class TargetTracking(Sequence):
 
     def _parse_next_trial(self):
         '''Get the required data from the generator'''
+        if self.state is None:
+            return
+        
         # yield idx, pts, disturbance, dis_trajectory, sample_rate :
         self.gen_index, self.targs, self.disturbance_trial, self.disturbance_path, self.sample_rate = self.next_trial # targs and disturbance are same length
 
