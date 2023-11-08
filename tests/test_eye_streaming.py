efe31836769747fa7108365de4552d3cd3753d84
Leo Scholl <leo.scholl@gmail.com>
2023-11-08 12:30:47 -0800

    add eye streaming test

---
+++ tests/test_eye_streaming.py
@@ -0,0 +1,12 @@
+import socket
+
+UDP_IP = "10.155.204.11"
+UDP_PORT = 11199
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
