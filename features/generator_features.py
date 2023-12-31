949f90c8681b8c3086449d7b053a8df215ec6f0b
katherineperks <leo.scholl@gmail.com>
2023-12-30 16:29:10 -0800

    Changed multi hold features

--- features/generator_features.py
+++ features/generator_features.py
@@ -74,9 +74,10 @@ class MultiHoldTime(traits.HasTraits):
     center targets and peripheral targets can have different hold times.
     '''
 
-    hold_time = traits.List([.2,], desc="Length of hold required at targets before next target appears. \
+    multi_hold_time = traits.List([.2,], desc="Length of hold required at targets before next target appears. \
         Can be a single number or a list of numbers to apply to each target in the sequence (center, out, etc.)")
-
+    exclude_parent_traits = ['hold_time']
+    
     def _test_hold_complete(self, time_in_state):
         '''
         Test whether the target is held long enough to declare the
@@ -88,11 +89,11 @@ class MultiHoldTime(traits.HasTraits):
             - Sensorized object moved to the required location
             - Manually triggered by experimenter
         '''
-        if len(self.hold_time) == 1:
-            hold_time = self.hold_time[0]
+        if len(self.multi_hold_time) == 1:
+            multi_hold_time = self.multi_hold_time[0]
         else:
-            hold_time = self.hold_time[self.target_index]
-        return time_in_state > hold_time
+            multi_hold_time = self.multi_hold_time[self.target_index]
+        return time_in_state > multi_hold_time
 
 class RandomDelay(traits.HasTraits):
     '''
